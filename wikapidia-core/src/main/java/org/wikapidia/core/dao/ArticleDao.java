ff8d4b74efade9eaadeef7eb9b1abcca2520a33b
shilad <shilad@gmail.com>
2013-06-03 15:12:34 -0500

    Initial test

---
+++ wikapidia-core/src/main/java/org/wikapidia/core/dao/ArticleDao.java
@@ -0,0 +1,11 @@
+package org.wikapidia.core.dao;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: shilad
+ * Date: 6/3/13
+ * Time: 3:11 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ArticleDao {
+}
