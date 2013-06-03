b9b5778a855253e2d091e2213d2c9343864ad110
shilad <shilad@gmail.com>
2013-06-03 15:22:42 -0500

    Added h2 dependency

--- wikapidia-core/src/main/java/org/wikapidia/core/dao/ArticleDao.java
+++ wikapidia-core/src/main/java/org/wikapidia/core/dao/ArticleDao.java
@@ -1,8 +1,14 @@
 package org.wikapidia.core.dao;
 
+import org.wikapidia.core.domain.Article;
+
 import static org.jooq.impl.DSL.*;
+
 
 /**
  */
 public class ArticleDao {
+    public Article get(int wpId) {
+        return null;
+    }
 }
