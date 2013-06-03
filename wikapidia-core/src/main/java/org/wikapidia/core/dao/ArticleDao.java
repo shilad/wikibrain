53382d34822e79b4f4c1c416ebfdcc846b59eb4c
shilad <shilad@gmail.com>
2013-06-03 15:19:27 -0500

    Added jooq dependency

--- wikapidia-core/src/main/java/org/wikapidia/core/dao/ArticleDao.java
+++ wikapidia-core/src/main/java/org/wikapidia/core/dao/ArticleDao.java
@@ -1,11 +1,8 @@
 package org.wikapidia.core.dao;
 
+import static org.jooq.impl.DSL.*;
+
 /**
- * Created with IntelliJ IDEA.
- * User: shilad
- * Date: 6/3/13
- * Time: 3:11 PM
- * To change this template use File | Settings | File Templates.
  */
 public class ArticleDao {
 }
