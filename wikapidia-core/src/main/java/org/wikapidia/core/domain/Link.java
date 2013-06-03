701dc171a9884397eabc3540ffa66c3997274376
Ari Weiland <math1man@gmail.com>
2013-06-03 15:57:42 -0500

    Push test

--- wikapidia-core/src/main/java/org/wikapidia/core/domain/Link.java
+++ wikapidia-core/src/main/java/org/wikapidia/core/domain/Link.java
@@ -1,11 +1,7 @@
 package org.wikapidia.core.domain;
 
 /**
- * Created with IntelliJ IDEA.
- * User: shilad
- * Date: 6/3/13
- * Time: 3:10 PM
- * To change this template use File | Settings | File Templates.
  */
 public class Link {
+
 }
