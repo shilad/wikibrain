cfb53be36a1bcb30f0b583fbb3cc1bbb0cf01e7d
Ari Weiland <math1man@gmail.com>
2013-06-03 16:49:12 -0500

    Merge branch 'master' of https://github.com/shilad/wikapidia
    Conflicts:
    	wikapidia-core/pom.xml
    	wikapidia-core/src/main/java/org/wikapidia/core/domain/Article.java
    	wikapidia-core/src/main/java/org/wikapidia/core/domain/Concept.java
    	wikapidia-core/src/main/java/org/wikapidia/core/domain/Link.java

--- wikapidia-core/src/main/java/org/wikapidia/core/domain/Concept.java
+++ wikapidia-core/src/main/java/org/wikapidia/core/domain/Concept.java
@@ -1,17 +1,10 @@
 package org.wikapidia.core.domain;
 
-/**
-<<<<<<< HEAD
- */
-public class Concept {
+import java.util.ArrayList;
 
-=======
- * Created with IntelliJ IDEA.
- * User: shilad
- * Date: 6/3/13
- * Time: 3:10 PM
- * To change this template use File | Settings | File Templates.
+/**
  */
 public class Concept {
->>>>>>> 374138b37deb19a7c6f2588b96affc7f27adb3bf
+    private int pageID;
+    private ArrayList
 }
--- wikapidia-core/src/main/java/org/wikapidia/core/domain/Link.java
+++ wikapidia-core/src/main/java/org/wikapidia/core/domain/Link.java
@@ -3,37 +3,14 @@ package org.wikapidia.core.domain;
 /**
  */
 public class Link {
-    private String myText;
-    private int myID;
-    private boolean mySubsec;
+    private String text;
+    private int pageID;
+    private boolean subsec;
 
-    public Link(String text, int id, boolean subsec) {
-        myText = text;
-        myID = id;
-        mySubsec = subsec;
+    public Link(String t, int id, boolean sub) {
+        text = t;
+        pageID = id;
+        subsec = sub;
     }
 
-    public String getMyText() {
-        return myText;
-    }
-
-    public void setMyText(String myText) {
-        this.myText = myText;
-    }
-
-    public int getMyID() {
-        return myID;
-    }
-
-    public void setMyID(int myID) {
-        this.myID = myID;
-    }
-
-    public boolean isMySubsec() {
-        return mySubsec;
-    }
-
-    public void setMySubsec(boolean mySubsec) {
-        this.mySubsec = mySubsec;
-    }
 }
