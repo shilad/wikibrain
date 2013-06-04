6b2452ac5482b9c5b3d99939b02efdbcd53822d0
Matt <loggerisms@gmail.com>
2013-06-04 14:14:57 -0500

    dao

--- wikapidia-core/src/main/java/org/wikapidia/core/dao/ArticleDao.java
+++ wikapidia-core/src/main/java/org/wikapidia/core/dao/ArticleDao.java
@@ -1,12 +1,42 @@
 package org.wikapidia.core.dao;
 
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
 import org.wikapidia.core.model.Article;
-
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import org.wikapidia.core.jooq.Tables;
 
 /**
  */
 public class ArticleDao {
     public Article get(int wpId) {
-        return null;
+        try{
+            Connection conn = connect();
+            DSLContext context = DSL.using(conn, SQLDialect.H2);
+            Record record = context.select().from(Tables.ARTICLE).where(Tables.ARTICLE.ID.equal(wpId)).fetchOne();
+            Article a = new Article(
+                record.getValue(Tables.ARTICLE.ID),
+                record.getValue(Tables.ARTICLE.TITLE),
+                record.getValue(Tables.ARTICLE.NS),
+                record.getValue(Tables.ARTICLE.PTYPE)
+            );
+            return null;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
     }
+
+    private Connection connect()throws Exception{
+        Class.forName("org.h2.Driver");
+        Connection conn = DriverManager.getConnection("tmp/maven-test");
+        return conn;
+    }
+
 }
