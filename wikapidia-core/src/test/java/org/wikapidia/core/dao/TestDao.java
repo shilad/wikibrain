dbacb496d1c17c4e35643d2ef0104d4e1c20bbb2
Rebecca Gold <rgold1@macalester.edu>
2013-06-05 13:10:41 -0500

    Merge branch 'master' of https://github.com/math1man/wikapidia
    Conflicts:
    	wikapidia-core/src/main/java/org/wikapidia/core/dao/ArticleDao.java
    	wikapidia-core/src/main/java/org/wikapidia/core/dao/ConceptDao.java

--- wikapidia-core/src/test/java/org/wikapidia/core/dao/TestDao.java
+++ wikapidia-core/src/test/java/org/wikapidia/core/dao/TestDao.java
@@ -1,24 +1,15 @@
 package org.wikapidia.core.dao;
 
-import com.jolbox.bonecp.BoneCP;
-import com.jolbox.bonecp.BoneCPConfig;
+
 import com.jolbox.bonecp.BoneCPDataSource;
-import junit.framework.Test;
+import org.junit.Test;
 import org.wikapidia.core.model.Article;
 
 import java.io.File;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 
-/**
- * Created with IntelliJ IDEA.
- * User: research
- * Date: 6/5/13
- * Time: 11:43 AM
- * To change this template use File | Settings | File Templates.
- */
 public class TestDao {
     @Test
     public void testArticle() throws ClassNotFoundException, IOException, SQLException {
@@ -27,15 +18,38 @@ public class TestDao {
         tmpDir.delete();
         tmpDir.deleteOnExit();
         tmpDir.mkdirs();
-        try {
-            BoneCPDataSource ds = new BoneCPDataSource();
-            ds.setJdbcUrl("jdbc:h2:"+new File(tmpDir,"db").getAbsolutePath());
-            ds.setUsername("sa");
-            ds.setPassword("");
-            ArticleDao ad = new ArticleDao(ds);
-            Article article = new Article(1,"test", Article.NameSpace.MAIN,Article.PageType.STANDARD);
-            ad.save(article);
+        BoneCPDataSource ds = new BoneCPDataSource();
+        ds.setJdbcUrl("jdbc:h2:"+new File(tmpDir,"db").getAbsolutePath());
+        ds.setUsername("sa");
+        ds.setPassword("");
+        ArticleDao ad = new ArticleDao(ds);
+        Article article = new Article(1,"test", Article.NameSpace.MAIN,Article.PageType.STANDARD);
+        ad.save(article);
+        Article saved = ad.get(1);
+        assert (article.getId()==saved.getId());
+        assert (article.getTitle().equals(saved.getTitle()));
+        assert (article.getNs().equals(saved.getNs()));
+        assert (article.getType().equals(saved.getType()));
+        List<Article> articles = ad.query("test");
+        assert (articles.size()==1);
+        assert (articles.get(0).getId()==1);
+        assert (articles.get(0).getTitle().equals(article.getTitle()));
+        assert (articles.get(0).getNs().equals(article.getNs()));
+        assert (articles.get(0).getType().equals(article.getType()));
+        articles = ad.query(Article.NameSpace.MAIN);
+        assert (articles.size()==1);
+        assert (articles.get(0).getId()==1);
+        assert (articles.get(0).getTitle().equals(article.getTitle()));
+        assert (articles.get(0).getNs().equals(article.getNs()));
+        assert (articles.get(0).getType().equals(article.getType()));
+        articles = ad.query("test",Article.NameSpace.MAIN);
+        assert (articles.size()==1);
+        assert (articles.get(0).getId()==1);
+        assert (articles.get(0).getTitle().equals(article.getTitle()));
+        assert (articles.get(0).getNs().equals(article.getNs()));
+        assert (articles.get(0).getType().equals(article.getType()));
+        articles = ad.query("wrong");
+        assert (articles==null);
 
-        }
     }
 }
