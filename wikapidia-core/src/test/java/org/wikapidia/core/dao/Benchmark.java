2371447a7359b9a241b3cb63620c274d60c8e8b9
yli2 <yli2@macalester.edu>
2013-06-06 14:26:17 -0500

    Merge branch 'master' of https://github.com/mlesicko/wikapidia
    Conflicts:
    	wikapidia-core/src/test/java/org/wikapidia/core/dao/Benchmark.java

--- wikapidia-core/src/test/java/org/wikapidia/core/dao/Benchmark.java
+++ wikapidia-core/src/test/java/org/wikapidia/core/dao/Benchmark.java
@@ -17,7 +17,7 @@ public class Benchmark {
     private int numLinks = 1000000;
     private int titleLength = 10;
     private int textMinLength = 30;
-    private int textMaxLength = 3000;
+    private int textMaxLength = 2500;
     boolean shouldBuildDb = false;
     boolean shouldBuildArticleDb = false;
 
