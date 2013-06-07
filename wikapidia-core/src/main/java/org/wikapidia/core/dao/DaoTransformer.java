9333f127e1374e526839cbf8697e93ad1dbc999c
Ari Weiland <math1man@gmail.com>
2013-06-07 09:45:02 -0500

    Merge branch 'master' of https://github.com/mlesicko/wikapidia
    Conflicts:
    	wikapidia-core/src/test/java/org/wikapidia/core/dao/Benchmark.java

--- wikapidia-core/src/main/java/org/wikapidia/core/dao/DaoTransformer.java
+++ wikapidia-core/src/main/java/org/wikapidia/core/dao/DaoTransformer.java
@@ -1,11 +1,6 @@
 package org.wikapidia.core.dao;
 
 /**
- * Created with IntelliJ IDEA.
- * User: logger
- * Date: 6/6/13
- * Time: 4:04 PM
- * To change this template use File | Settings | File Templates.
  */
 public class DaoTransformer {
 }
--- wikapidia-core/src/main/java/org/wikapidia/core/dao/WikapidiaIterable.java
+++ wikapidia-core/src/main/java/org/wikapidia/core/dao/WikapidiaIterable.java
@@ -3,11 +3,6 @@ package org.wikapidia.core.dao;
 import java.util.Iterator;
 
 /**
- * Created with IntelliJ IDEA.
- * User: logger
- * Date: 6/6/13
- * Time: 3:44 PM
- * To change this template use File | Settings | File Templates.
  */
 public class WikapidiaIterable<E> implements Iterable<E> {
     @Override
