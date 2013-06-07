9a861f96394563a7b046e44f0a6cf6fd8cf36b09
Ari Weiland <math1man@gmail.com>
2013-06-07 14:40:14 -0500

    Added junit to pom.xml

---
+++ wikapidia-parser/src/test/java/org/wikapidia/parser/TestWikiTextDumpParser.java
@@ -0,0 +1,11 @@
+package org.wikapidia.parser;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: houde
+ * Date: 6/7/13
+ * Time: 2:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestWikiTextDumpParser {
+}
