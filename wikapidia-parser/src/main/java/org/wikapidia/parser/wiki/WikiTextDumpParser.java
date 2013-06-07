b7448e291623b412390e23e08bd83687079bbc07
Ari Weiland <math1man@gmail.com>
2013-06-07 14:33:38 -0500

    Modified WikiTextDumpParser

--- wikapidia-parser/src/main/java/org/wikapidia/parser/wiki/WikiTextDumpParser.java
+++ wikapidia-parser/src/main/java/org/wikapidia/parser/wiki/WikiTextDumpParser.java
@@ -2,15 +2,20 @@ package org.wikapidia.parser.wiki;
 
 import org.wikapidia.core.WikapidiaException;
 import org.wikapidia.core.lang.LanguageInfo;
+import org.wikapidia.parser.DumpSplitter;
 import org.wikapidia.parser.xml.DumpPageXmlParser;
 import org.wikapidia.parser.xml.PageXml;
 
 import java.io.File;
 import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  */
 public class WikiTextDumpParser {
+    public static final Logger LOG = Logger.getLogger(WikiTextDumpParser.class.getName());
+
     private final File file;
     private final LanguageInfo language;
     private final DumpPageXmlParser dpxp;
@@ -23,12 +28,17 @@ public class WikiTextDumpParser {
         this.wtp = new WikiTextParser(language);
     }
 
-    public void iterate(ParserVisitor visitor) throws WikapidiaException {
+    public void parse(ParserVisitor visitor) {
         // use the dump page xml parser to iterate over the dump
         Iterator<PageXml> dumpIterator = dpxp.iterator();
         // call the wiki text parser for each article with the visitor
         while (dumpIterator.hasNext()) {
-            wtp.parse(dumpIterator.next(), visitor);
+            try {
+                wtp.parse(dumpIterator.next(), visitor);
+            }
+            catch (WikapidiaException e) {
+                LOG.log(Level.WARNING, "parsing of " + file + " failed:", e);
+            }
         }
     }
 }
