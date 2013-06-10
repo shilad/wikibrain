56852bc6f5798961ba481ba4e551796ace72c273
yli2 <yli2@macalester.edu>
2013-06-10 09:33:25 -0500

    Merge branch 'master' of https://github.com/mlesicko/wikapidia
    Conflicts:
    	wikapidia-core/src/test/java/org/wikapidia/core/dao/Benchmark.java

--- wikapidia-core/src/main/java/org/wikapidia/core/model/LocalLink.java
+++ wikapidia-core/src/main/java/org/wikapidia/core/model/LocalLink.java
@@ -1,11 +1,54 @@
 package org.wikapidia.core.model;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import java.util.List;
+
 /**
- * Created with IntelliJ IDEA.
- * User: ilps
- * Date: 6/7/13
- * Time: 12:01 PM
- * To change this template use File | Settings | File Templates.
  */
 public class LocalLink {
+
+    private final Integer localId;
+    private final Integer location;
+    private final Boolean isParseable;
+    private final LocationType locType;
+
+    private static enum LocationType {FIRST_PARA, FIRST_SEC, NONE};
+
+    public boolean isRedId(int id){
+        return (id < 0);
+    }
+
+    public LocalLink(Integer localID, Integer location, Boolean parseable, LocationType locType) {
+        this.localId = localID;
+        this.location = location;
+        isParseable = parseable;
+        this.locType = locType;
+    }
+
+    /**
+     * Returns the outlink or inlink IDs of the pivot page.
+     * @return
+     */
+    public Integer getLocalId() {
+        return localId;
+    }
+
+    /**
+     * Returns the byte location of the beginning of the lin
+     * @return
+     */
+    public Integer getLocation() {
+        return location;
+    }
+
+    /**
+     * Returns the parseability of the link.
+     * @return
+     */
+    public Boolean getIsParseable() {
+        return isParseable;
+    }
+
+
 }
