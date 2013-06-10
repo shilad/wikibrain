42eb8caca569ad022e0eaf5a36493b56c34d374d
Rebecca Gold <rgold1@macalester.edu>
2013-06-10 10:48:24 -0500

    Updates to LocalPageDao (possibly debunked by Matt)
    JavaDocs for PageType

--- wikapidia-core/src/main/java/org/wikapidia/core/model/PageType.java
+++ wikapidia-core/src/main/java/org/wikapidia/core/model/PageType.java
@@ -1,5 +1,10 @@
 package org.wikapidia.core.model;
 
+/**
+ * A PageType defines the kind Wikipidia page a given page is.
+ * The PageType contains the NameSpace enum that specifies what namespace a given PageType is in.
+ * This allows us to query by NameSpace or by PageType and filter out the distinctions.
+ */
 public enum PageType {
 
     ARTICLE(NameSpace.MAIN), REDIRECT(NameSpace.MAIN), DISAMBIG(NameSpace.MAIN),
@@ -33,22 +38,44 @@ public enum PageType {
 
     NameSpace namespace;
 
+    /**
+     * This will construct a PageType that specifies the NameSpace that the PageType is in.
+     * @param ns The NameSpace of PageType.
+     */
     PageType(NameSpace ns) {
         this.namespace = ns;
     }
 
+    /**
+     *
+     * @return The NameSpace of a given PageType
+     */
     public NameSpace getNamespace() {
         return namespace;
     }
 
+    /**
+     * Every PageType has a corresponding Talk-type page.
+     * @return This method will let you know if a given PageType is a Talk-type page.
+     */
     public boolean isTalk() {
         return (!(namespace==NameSpace.SPECIAL) && namespace.getValue()%2 == 1);
     }
 
+    /**
+     *
+     * @return If the given PageType is in the Main NameSpace
+     */
     public boolean isMainNs() {
         return namespace == NameSpace.MAIN;
     }
 
+    /**
+     * This is the NameSpace enum.
+     * Each PageType has a specified NameSpace.
+     * The main distinctions come from the three different types of
+     * Main-NameSpace pages (Article, Redirect and Disambig).
+     */
     public static enum NameSpace {
         MAIN(0), TALK(1),
         USER(2), USER_TALK(3),
@@ -68,14 +95,28 @@ public enum PageType {
 
         private int value;
 
+        /**
+         * Constructor for NameSpace.
+         * @param value The numeric value of the NameSpace as defined by Wikipedia.
+         */
         private NameSpace(int value) {
             this.value = value;
         }
 
+        /**
+         *
+         * @return The numeric value of the NameSpace as defined by Wikipedia.
+         */
         public int getValue() {
             return value;
         }
 
+        /**
+         * This method lets you figure out what NameSpace is being specified when given
+         * an numeric value for that NameSpace.
+         * @param value The numeric value of the NameSpace as defined by Wikipedia.
+         * @return The corrisponding NameSpace if it exists, or null.
+         */
         static public NameSpace intToNS(int value){
             if(value>=0 && value<16) return NameSpace.values()[value];
             for (NameSpace v : NameSpace.values()){
