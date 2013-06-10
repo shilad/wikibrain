713cff48bf6027eda7bd4261a387f010879657d1
Ari Weiland <math1man@gmail.com>
2013-06-10 10:56:55 -0500

    temp

---
+++ wikapidia-core/src/main/java/org/wikapidia/core/model/LocalLink.java
@@ -0,0 +1,16 @@
+package org.wikapidia.core.model;
+
+import org.wikapidia.core.lang.Language;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: houde
+ * Date: 6/10/13
+ * Time: 10:52 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LocalLink {
+    private Language language;
+    private String text;
+
+}
