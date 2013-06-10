2d2a647ca12d3de6609bc84ddf537dcc91f9dae5
Matt <loggerisms@gmail.com>
2013-06-10 14:11:21 -0500

    Category DAOs

--- wikapidia-core/src/main/java/org/wikapidia/core/dao/LocalCategoryDao.java
+++ wikapidia-core/src/main/java/org/wikapidia/core/dao/LocalCategoryDao.java
@@ -1,4 +1,50 @@
 package org.wikapidia.core.dao;
 
-public class LocalCategoryDao<LocalCategory> extends LocalPageSqlDao {
+import org.jooq.Record;
+import org.wikapidia.core.WikapidiaException;
+import org.wikapidia.core.jooq.Tables;
+import org.wikapidia.core.lang.Language;
+import org.wikapidia.core.lang.LanguageInfo;
+import org.wikapidia.core.model.LocalCategory;
+import org.wikapidia.core.model.PageType;
+import org.wikapidia.core.model.Title;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+public abstract class LocalCategoryDao extends LocalPageDao<LocalCategory> {
+    public LocalCategoryDao(DataSource dataSource) throws SQLException {
+        super(dataSource);
+    }
+
+    public abstract LocalCategory getByPageId(Language language, int pageId) throws DaoException;
+
+    public abstract LocalCategory getByTitle(Language language, Title title, PageType ns) throws DaoException;
+
+    /**
+     * Build a LocalCategory from a database record representation
+     * @param record a database record
+     * @return a LocalCategory representation of the given database record
+     * @throws org.wikapidia.core.WikapidiaException if the record is not a Category
+     */
+    protected LocalCategory buildLocalCategory(Record record) throws DaoException {
+        if (record == null) {
+            return null;
+        }
+        Language lang = Language.getById(record.getValue(Tables.LOCAL_PAGE.LANG_ID));
+        Title title = new Title(
+                record.getValue(Tables.LOCAL_PAGE.TITLE), true,
+                LanguageInfo.getByLanguage(lang));
+        PageType ptype = PageType.values()[record.getValue(Tables.LOCAL_PAGE.PAGE_TYPE)];
+        if (!ptype.equals(PageType.CATEGORY)){
+            throw new DaoException("tried to get Category, but found "+ptype.name());
+        }
+        else {
+            return new LocalCategory(
+                    lang,
+                    record.getValue(Tables.LOCAL_PAGE.PAGE_ID),
+                    title
+            );
+        }
+    }
 }
--- wikapidia-core/src/main/java/org/wikapidia/core/dao/LocalPageDao.java
+++ wikapidia-core/src/main/java/org/wikapidia/core/dao/LocalPageDao.java
@@ -1,21 +1,37 @@
 package org.wikapidia.core.dao;
 
+import org.jooq.SQLDialect;
 import org.wikapidia.core.lang.Language;
 import org.wikapidia.core.model.LocalPage;
 import org.wikapidia.core.model.PageType;
 import org.wikapidia.core.model.Title;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 public abstract class LocalPageDao<T extends LocalPage> {
+    protected final SQLDialect dialect;
+    protected DataSource ds;
 
-    public abstract T getByPageId(Language language, int pageId);
+    public LocalPageDao(DataSource dataSource) throws SQLException {
+        ds = dataSource;
+        Connection conn = ds.getConnection();
+        try {
+            this.dialect = JooqUtils.dialect(conn);
+        } finally {
+            conn.close();
+        }
+    }
 
-    public abstract T getByTitle(Language language, Title title, PageType ns);
+    public abstract T getByPageId(Language language, int pageId) throws DaoException;
 
-    public Map<Integer, T> getByIds(Language language, Collection<Integer> pageIds) {
+    public abstract T getByTitle(Language language, Title title, PageType ns) throws DaoException;
+
+    public Map<Integer, T> getByIds(Language language, Collection<Integer> pageIds) throws DaoException{
         Map<Integer, T> map = new HashMap<Integer,T>();
         for (int id : pageIds){
             map.put(id,getByPageId(language,id));
@@ -23,7 +39,7 @@ public abstract class LocalPageDao<T extends LocalPage> {
         return map;
     }
 
-    public Map<Title, T> getbyTitles(Language language, Collection<Title> titles, PageType ns){
+    public Map<Title, T> getbyTitles(Language language, Collection<Title> titles, PageType ns) throws DaoException{
         Map<Title, T> map = new HashMap<Title, T>();
         for (Title title : titles){
             map.put(title, getByTitle(language, title, ns));
