86800286f72e7e90462a25c80d4a82b201456b86
Ari Weiland <math1man@gmail.com>
2013-06-11 11:16:10 -0500

    Deletes and LocalLink change

--- wikapidia-core/src/main/java/org/wikapidia/core/model/LocalLink.java
+++ wikapidia-core/src/main/java/org/wikapidia/core/model/LocalLink.java
@@ -8,7 +8,9 @@ public class LocalLink {
 
     private final Language language;
     private final String anchorText;
-    private final int localId;
+    private final int sourceId;
+    private final int destId;
+    private final boolean isOutlink;
     private final int location;
     private final boolean isParseable;
     private final LocationType locType;
@@ -19,12 +21,14 @@ public class LocalLink {
         return (id < 0);
     }
 
-    public LocalLink(Language language, String anchorText, int localId, int location, boolean isParseable, LocationType locType) {
+    public LocalLink(Language language, String anchorText, int sourceId, int destId, boolean outlink, int location, boolean parseable, LocationType locType) {
         this.language = language;
         this.anchorText = anchorText;
-        this.localId = localId;
+        this.sourceId = sourceId;
+        this.destId = destId;
+        isOutlink = outlink;
         this.location = location;
-        this.isParseable = isParseable;
+        isParseable = parseable;
         this.locType = locType;
     }
 
@@ -45,14 +49,28 @@ public class LocalLink {
     }
 
     /**
-     * Returns the outlink or inlink IDs of the pivot page.
-     * @return
+     * The sourceId, destId, and isOutlink fields are all interrelated.
+     * The ID fields give the local ID of the source and destination pages, respectivley,
+     * while the isOutlink boolean determines whether the LocalLink was instantiated
+     * as an outlink or an inlink.  This determines which parameter is returned by this
+     * getLocalId method, which returns sourceId if isOutlink is true, and destId if not.
+     *
+     * @return sourceId or destId, depending on the field isOutlink
      */
     public int getLocalId() {
-        return localId;
+        return (isOutlink ? sourceId : destId);
     }
 
     /**
+     * Returns whether or not this link is an outlink. Otherwise, it is an inlink.
+     * @return
+     */
+    public boolean isOutlink() {
+        return isOutlink;
+    }
+
+    /**
+     *
      * Returns the byte location of the beginning of the link.
      * @return
      */
