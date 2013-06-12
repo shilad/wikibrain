088d7bc458bc2d5d30ba3c3918c964294b615091
Rebecca Gold <rgold1@macalester.edu>
2013-06-12 11:34:28 -0500

    RawPageDao and schema updated to match raw-page-schema

--- wikapidia-core/src/main/java/org/wikapidia/core/dao/RawPageDao.java
+++ wikapidia-core/src/main/java/org/wikapidia/core/dao/RawPageDao.java
@@ -1,17 +1,28 @@
 package org.wikapidia.core.dao;
 
 import org.wikapidia.core.lang.Language;
+import org.wikapidia.core.model.PageType;
 import org.wikapidia.core.model.RawPage;
+import org.wikapidia.core.model.Title;
+
+import java.util.Date;
 
 public interface RawPageDao extends Loader<RawPage> {
 
     /**
-     * Returns the RawPage associated with the page given local page.
+     *
      * @param language
      * @param localPageId
+     * @param revisionId
+     * @param body
+     * @param title
+     * @param lastEdit
+     * @param pageType
      * @return
+     * @throws DaoException
      */
-    public RawPage get(Language language, int localPageId) throws DaoException;
+    public RawPage get(Language language, int localPageId, int revisionId, String body, Title title,
+                       Date lastEdit, PageType pageType) throws DaoException;
 
     /**
      * Returns the body (i.e. wikitext) of a particular local page.
