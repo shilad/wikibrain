2ff06c5264aff8d1c662612fad7218ae62f7ec74
shilad <shilad@gmail.com>
2013-06-14 10:50:41 -0500

    Redirect branch

---
+++ wikapidia-parser/src/main/java/org/wikapidia/parser/wiki/RedirectParser.java
@@ -0,0 +1,41 @@
+package org.wikapidia.parser.wiki;
+
+import org.wikapidia.core.lang.Language;
+import org.wikapidia.core.lang.LanguageInfo;
+import org.wikapidia.core.model.Title;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ */
+public class RedirectParser {
+    private final LanguageInfo language;
+
+    public RedirectParser(Language language) {
+        this.language = LanguageInfo.getByLanguage(language);
+    }
+
+    private boolean isRedirect(String body) {
+        return extractSingleString(language.getRedirectPattern(), body, 1) != null;
+    }
+
+    private Title getRedirect(String body) {
+        String title =  extractSingleString(language.getRedirectPattern(), body, 1);
+        return new Title(title, language);
+    }
+
+    private static String extractSingleString(Pattern patternToMatch, String body, int matchNum){
+        if (patternToMatch == null || body == null) {
+            return null;
+        }
+        Matcher matcher = patternToMatch.matcher(body);
+        int counter = 0;
+        String curGroup = null;
+        while (matcher.find() && counter < matchNum){
+            curGroup = matcher.group(1);
+            counter++;
+        }
+        return curGroup;
+    }
+}
