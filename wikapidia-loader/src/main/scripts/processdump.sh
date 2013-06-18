2b66a2a84fffcb0226f28f98ea57ce8ead68d69b
shilad <shilad@gmail.com>
2013-06-18 11:01:33 -0500

    Placeholders for shell scripts

---
+++ wikapidia-loader/src/main/scripts/processdump.sh
@@ -0,0 +1,8 @@
+# Invoke this using ./src/main/processdump.sh
+#
+# Import environment parameters from conf.sh
+# Import helpers from utils.sh
+#
+# Compile
+# Update CLASSPATH from the classpath file
+# java org.wikapidia.dao.loader.LoaderMain $@
