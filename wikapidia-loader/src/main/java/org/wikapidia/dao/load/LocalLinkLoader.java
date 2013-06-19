dda5099022d7984d5b2aec52c796a2c0f07456ad
Rebecca Gold <rgold1@macalester.edu>
2013-06-19 16:38:36 -0500

    catMemDao and other fun things about getting links and categories

--- wikapidia-loader/src/main/java/org/wikapidia/dao/load/LocalLinkLoader.java
+++ wikapidia-loader/src/main/java/org/wikapidia/dao/load/LocalLinkLoader.java
@@ -2,21 +2,26 @@ package org.wikapidia.dao.load;
 
 import org.wikapidia.core.WikapidiaException;
 import org.wikapidia.core.dao.DaoException;
+import org.wikapidia.core.dao.LocalCategoryMemberDao;
 import org.wikapidia.core.dao.LocalLinkDao;
 import org.wikapidia.core.dao.LocalPageDao;
-import org.wikapidia.core.model.LocalLink;
-import org.wikapidia.core.model.LocalPage;
-import org.wikapidia.core.model.RawPage;
+import org.wikapidia.core.lang.Language;
+import org.wikapidia.core.lang.LanguageInfo;
+import org.wikapidia.core.model.*;
 import org.wikapidia.parser.wiki.ParsedLink;
 import org.wikapidia.parser.wiki.ParserVisitor;
 
 /**
  */
 public class LocalLinkLoader extends ParserVisitor {
-    private final LocalLinkDao dao;
+    private final LocalLinkDao linkDao;
+    private final LocalPageDao pageDao;
+    private final LocalCategoryMemberDao catMemDao;
 
-    public LocalLinkLoader(LocalLinkDao dao) {
-        this.dao = dao;
+    public LocalLinkLoader(LocalLinkDao linkDao, LocalPageDao pageDao, LocalCategoryMemberDao catMemDao) {
+        this.linkDao = linkDao;
+        this.pageDao = pageDao;
+        this.catMemDao = catMemDao;
     }
 
     @Override
@@ -28,19 +33,52 @@ public class LocalLinkLoader extends ParserVisitor {
             } else if (link.location.getSection() == 0) {
                 loc = LocalLink.LocationType.FIRST_SEC;
             }
-            dao.save(
-                    new LocalLink(
-                            link.target.getLanguage(),
-                            link.text,
-                            link.location.getXml().getPageId(),
-                            link.target.hashCode(), // FIXME.. YUCK!
-                            true,
-                            link.location.getLocation(),
-                            true,
-                            loc
+            Language lang = link.target.getLanguage();
+            LanguageInfo langInfo = LanguageInfo.getByLanguage(lang);
+            if(isCategory(link.text, langInfo)){
+                catMemDao.save(
+                        new LocalCategoryMember(pageDao.getIdByTitle(Title.canonicalize(link.text, langInfo),
+                                lang, NameSpace.CATEGORY),
+                                link.location.getXml().getPageId(),
+                                lang)
+                );
+            }
+            else{
+                if(isLinkToCategory(link.text, langInfo))
+                    link.text = link.text.substring(1,link.text.length());
+                Title linkTitle = new Title(link.text, langInfo);
+                linkDao.save(
+                        new LocalLink(
+                                lang,
+                                link.text,
+                                link.location.getXml().getPageId(),
+                                pageDao.getIdByTitle(linkTitle.getCanonicalTitle(), lang, linkTitle.getNamespace()),
+                                true,
+                                link.location.getLocation(),
+                                true,
+                                loc
                         ));
+            }
         } catch (DaoException e) {
             throw new WikapidiaException(e);
         }
+    }
+
+    public boolean isCategory(String link, LanguageInfo languageInfo){
+        for(String categoryName : languageInfo.getCategoryNames()){
+            if(link.substring(categoryName.length()+1).toLowerCase().equals(categoryName+":")){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isLinkToCategory(String link, LanguageInfo languageInfo){
+        for(String categoryName : languageInfo.getCategoryNames()){
+            if(link.substring(categoryName.length()+2).toLowerCase().equals(":" + categoryName+":")){
+                return true;
+            }
+        }
+        return false;
     }
 }
