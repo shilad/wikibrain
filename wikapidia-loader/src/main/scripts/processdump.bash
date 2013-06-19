18b5b5707b5659a9cd7785d8323236bc99d43004
Ari Weiland <math1man@gmail.com>
2013-06-19 11:47:26 -0500

    ...

--- wikapidia-loader/src/main/scripts/processdump.bash
+++ wikapidia-loader/src/main/scripts/processdump.bash
@@ -19,7 +19,7 @@ read < ./target/classpath REMOTE_CLASS_PATH
 
 CLASS_PATH="${REMOTE_CLASS_PATH}:$LOCAL_CLASS_PATH"
 
-echo $CLASS_PATH
+echo "$CLASS_PATH"
 
 java -cp "$CLASS_PATH" $JVM_OPTS org.wikapidia.dao.load.DumpLoader "$@"
 java -cp "$CLASS_PATH" $JVM_OPTS org.wikapidia.dao.load.ConceptLoader "$@"
