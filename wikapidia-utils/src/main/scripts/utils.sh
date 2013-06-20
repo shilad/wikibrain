8183352296d214b2aa4a9bfca97fa9fd41e8d0fe
shilad <shilad@gmail.com>
2013-06-20 10:28:25 -0500

    Cleaning up scripts.

--- wikapidia-utils/src/main/scripts/utils.sh
+++ wikapidia-utils/src/main/scripts/utils.sh
@@ -7,9 +7,32 @@ function die() {
     exit 1
 }
 
+export WP_BASE=../
+export WP_CORE=$WP_BASE/wikapidia-core
+export WP_LOADER=$WP_BASE/wikapidia-loader
+export WP_MAPPER=$WP_BASE/wikapidia-mapper
+export WP_PARENT=$WP_BASE/wikAPIdia-parent
+export WP_PARSER=$WP_BASE/wikapidia-parser
+export WP_UTILS=$WP_BASE/wikapidia-utils
+
+if ! [ -d ${WP_BASE} ]; then
+    die "missing base directory ${WP_BASE}"
+fi
+
+for d in "${WP_CORE}" "${WP_LOADER}" "${WP_MAPPER}" "${WP_PARENT}" "${WP_PARSER}" "${WP_UTILS}"; do
+    if ! [ -d "$d" ]; then
+        die "missing module directory $d"
+    fi
+done
+
+
+function compileJooq() {
+    cat ../wikapidia-core/src/main/resources/db/*-schema.sql >
+}
+
 function compile() {
     echo compiling... &&
-    (cd ../wikAPIdia-parent && mvn compile ) ||
+    (cd ${WP_PARENT} && mvn compile ) ||
     die "compilation failed"
 
     echo finished compiling
@@ -31,6 +54,6 @@ function execClass() {
 
 # source all util scripts here, so that other scripts only
 # need to source this script to source everything
-source ../wikapidia-utils/src/main/scripts/conf.sh
+source ${WP_UTILS}/src/main/scripts/conf.sh
 
 
