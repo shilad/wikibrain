39766c7f0e894b1baa19801e890073ee8247b56a
shilad <shilad@gmail.com>
2013-06-20 16:59:02 -0500

    Shifting matrix package over in preparation for sr metrics.

--- wikapidia-utils/src/main/scripts/utils.sh
+++ wikapidia-utils/src/main/scripts/utils.sh
@@ -11,13 +11,15 @@ export WP_BASE=../
 export WP_CORE=$WP_BASE/wikapidia-core
 export WP_LOADER=$WP_BASE/wikapidia-loader
 export WP_MAPPER=$WP_BASE/wikapidia-mapper
+export WP_MATRIX=$WP_BASE/wikapidia-matrix
 export WP_PARENT=$WP_BASE/wikAPIdia-parent
 export WP_PARSER=$WP_BASE/wikapidia-parser
 export WP_UTILS=$WP_BASE/wikapidia-utils
 
+
 [ -d ${WP_BASE} ] || die "missing base directory ${WP_BASE}"
 
-for d in "${WP_CORE}" "${WP_LOADER}" "${WP_MAPPER}" "${WP_PARENT}" "${WP_PARSER}" "${WP_UTILS}"; do
+for d in "${WP_CORE}" "${WP_LOADER}" "${WP_MAPPER}" "${WP_PARENT}" "${WP_PARSER}" "${WP_UTILS}" "${WP_MATRIX}"; do
     [ -d "$d" ] || die "missing module directory $d"
 done
 
