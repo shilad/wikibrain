b6b160ebf04d5ab9b0c6e3146536bba6a8d5d035
Ari Weiland <math1man@gmail.com>
2013-06-20 14:22:32 -0500

    ...

--- wikapidia-core/src/main/scripts/codegen.sh
+++ wikapidia-core/src/main/scripts/codegen.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #
 # Runs the function to generate the jOOQ files
-# Requires no parameters
+# Requires no parameters, but must be run from the home directory
+# of any module.
 
 source ../wikapidia-utils/src/main/scripts/utils.sh &&
 compile ||
