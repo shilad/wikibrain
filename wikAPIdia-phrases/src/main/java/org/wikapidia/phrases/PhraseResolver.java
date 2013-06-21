6461f56cd401ad053c5d3948c926b8d768f4314e
shilad <shilad@gmail.com>
2013-06-21 12:57:12 -0500

    Working towards an SR module...

---
+++ wikAPIdia-phrases/src/main/java/org/wikapidia/phrases/PhraseResolver.java
@@ -0,0 +1,57 @@
+package org.wikapidia.phrases;
+
+import org.wikapidia.core.jooq.tables.UniversalPage;
+import org.wikapidia.core.lang.Language;
+import org.wikapidia.core.model.LocalPage;
+
+import java.util.LinkedHashMap;
+
+/**
+ * A phrase resolver supports two features:
+ *
+ * For a given phrase, returns the most common pages.
+ * For a given page, returns the most common phrases.
+ *
+ * If you want smarter, contex aware disambiguation, look at disambig in the sr module.
+ */
+public interface PhraseResolver {
+    /**
+     * Returns the most likely wikipedia pages for a phrase.
+     * @param language The language for the phrase and the returned LocalPages.
+     * @param phrase The phrase to be resolved.
+     * @param maxPages The maximum number of pages to be returned.
+     * @return An map from page to score, ordered by decreasing probability.
+     * The scores can be considered probabilities that sum to 1.0 across all possibilities.
+     */
+    public LinkedHashMap<LocalPage, Float> resolveLocal(Language language, String phrase, int maxPages);
+
+    /**
+     * Returns the most likely universal pages for a phrase.
+     * @param language The language for the phrase.
+     * @param phrase The phrase to be resolved.
+     * @param maxPages The maximum number of pages to be returned.
+     * @return An map from page to score, ordered by decreasing probability.
+     * The scores can be considered probabilities that sum to 1.0 across all possibilities.
+     */
+    public LinkedHashMap<UniversalPage, Float> resolveUniversal(Language language, String phrase, int algorithmId, int maxPages);
+
+    /**
+     * Returns the most descriptive phrases for a wikipedia page.
+     * @param language The language for the phrase and the returned LocalPages.
+     * @param page The page to be described.
+     * @param maxPhrases The maximum number of phrases to be returned.
+     * @return An map from phrase to score, ordered by decreasing probability.
+     * The scores can be considered probabilities that sum to 1.0 across all possibilities.
+     */
+    public LinkedHashMap<String, Float> describeLocal(Language language, LocalPage page, int maxPhrases);
+
+    /**
+     * Returns the most descriptive phrases for a universal page.
+     * @param language The language for the returned phrases.
+     * @param page The page to be described.
+     * @param maxPhrases The maximum number of phrases to be returned.
+     * @return An map from phrase to score, ordered by decreasing probability.
+     * The scores can be considered probabilities that sum to 1.0 across all possibilities.
+     */
+    public LinkedHashMap<String, Float> describeUniversal(Language language, UniversalPage page, int maxPhrases);
+}
