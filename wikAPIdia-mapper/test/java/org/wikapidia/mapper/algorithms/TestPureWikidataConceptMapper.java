d47486c0cd12a44004b42ae07d0e2b61b7a973f2
Brent Hecht <brenthechtjunk@gmail.com>
2013-06-25 16:07:43 -0500

    PureWikidataConceptMapper draft completed

---
+++ wikAPIdia-mapper/test/java/org/wikapidia/mapper/algorithms/TestPureWikidataConceptMapper.java
@@ -0,0 +1,29 @@
+//package org.wikapidia.mapper.algorithms;
+//
+//import org.junit.*;
+//import org.wikapidia.core.WikapidiaException;
+//import org.wikapidia.mapper.algorithms.PureWikidataConceptMapper;
+//
+///**
+// * Created with IntelliJ IDEA.
+// * User: bjhecht
+// * Date: 6/25/13
+// * Time: 2:33 PM
+// * To change this template use File | Settings | File Templates.
+// */
+//public class TestPureWikidataConceptMapper {
+//
+//    @Test
+//    public void testBasic(){
+//
+//        try {
+//
+//            String path = "/Users/bjhecht/Downloads/wikidatawiki-20130527-wb_items_per_site.sql";
+//
+//        } catch (WikapidiaException e) {
+//            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//        }
+//
+//
+//    }
+//}
