230dca1d38723eb9f99badcfb26bc73213e82909
shilad <shilad@gmail.com>
2013-06-26 20:45:26 -0500

    Merge remote-tracking branch 'remotes/origin/master' into category
    Conflicts:
    	wikapidia-loader/src/main/java/org/wikapidia/dao/load/RedirectLoader.java
    	wikapidia-loader/src/main/java/org/wikapidia/dao/load/WikiTextDumpLoader.java

--- wikapidia-loader/src/main/java/org/wikapidia/dao/load/WikiTextDumpLoader.java
+++ wikapidia-loader/src/main/java/org/wikapidia/dao/load/WikiTextDumpLoader.java
@@ -73,12 +73,8 @@ public class WikiTextDumpLoader {
             new HelpFormatter().printHelp("DumpLoader", options);
             return;
         }
-        if (cmd.getArgList().isEmpty()) {
-            System.err.println("No input files specified.");
-            new HelpFormatter().printHelp("WikiTextDumpLoader", options);
-            return;
-        }
-        File pathConf = cmd.hasOption("c") ? new File(cmd.getOptionValue('c')) : null;
+
+        File pathConf = new File(cmd.getOptionValue('c', null));
         Configurator conf = new Configurator(new Configuration(pathConf));
 
         List<String> languages;
