242dfef497b54791e3d907b7bbcda62ab3a02022
yli2 <yli2@macalester.edu>
2013-06-27 11:39:20 -0500

    Added scripts

--- wikAPIdia-download/src/main/scripts/filedownloader.sh
+++ wikAPIdia-download/src/main/scripts/filedownloader.sh
@@ -2,5 +2,5 @@
 
 source ../wikapidia-utils/src/main/scripts/utils.sh &&
 compile &&
-execClass org.wikapidia.download.RequestedLinkGetter $@ ||
+execClass org.wikapidia.download.FileDownloader $@ ||
 die "$0 failed"
