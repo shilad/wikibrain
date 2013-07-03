ad3c1a4c3aceef6772944018485ce38aeae087f0
Ari Weiland <math1man@gmail.com>
2013-07-03 10:47:05 -0500

    added LuceneLoader

---
+++ wikapidia-loader/src/main/java/org/wikapidia/dao/load/LuceneLoader.java
@@ -0,0 +1,11 @@
+package org.wikapidia.dao.load;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: houde
+ * Date: 7/3/13
+ * Time: 10:44 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LuceneLoader {
+}
