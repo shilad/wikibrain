25279216446308d515ace7e1385bc4d0885bb2f5
Ari Weiland <math1man@gmail.com>
2013-07-16 22:49:33 -0500

    ...

--- wikAPIdia-utils/src/main/scripts/conf.sh
+++ wikAPIdia-utils/src/main/scripts/conf.sh
@@ -3,4 +3,4 @@
 # Contains useful configuration variables
 
 export JAVA_OPTS="-Xmx4000M -server -ea"
-export LOCAL_CLASSPATH="../wikAPIdia-core/target/classes:../wikAPIdia-download/target/classes:../wikAPIdia-loader/target/classes:../wikAPIdia-lucene/target/classes:../wikAPIdia-mapper/target/classes:../wikAPIdia-matrix/target/classes:../wikAPIdia-parser/target/classes:../wikAPIdia-phrases/target/classes:../wikAPIdia-sr/target/classes:../wikAPIdia-phrases/target/classes:../wikAPIdia-utils/target/classes:"
+export LOCAL_CLASSPATH="../wikAPIdia-core/target/classes:../wikAPIdia-download/target/classes:../wikAPIdia-loader/target/classes:../wikAPIdia-lucene/target/classes:../wikAPIdia-mapper/target/classes:../wikAPIdia-matrix/target/classes:../wikAPIdia-parser/target/classes:../wikAPIdia-phrases/target/classes:../wikAPIdia-sr/target/classes:../wikAPIdia-utils/target/classes:"
