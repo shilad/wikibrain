3bc45e8828df124d2f108e222c3a94aeba9b338f
Ari Weiland <math1man@gmail.com>
2013-07-16 22:40:53 -0500

    ...

--- wikAPIdia-parser/src/test/resources/org/wikapidia/parser/pagelinks.sql
+++ wikAPIdia-parser/src/test/resources/org/wikapidia/parser/pagelinks.sql
@@ -19,7 +19,7 @@
 -- Table structure for table `pagelinks`
 --
 
-DROP TABLE IF EXISTS pagelinks;
+DROP TABLE IF EXISTS `pagelinks`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pagelinks` (
