1bd2acd564d39301ab1a4e7c089baa7cf50559ae
Matt <loggerisms@gmail.com>
2013-08-09 13:55:46 -0500

    improved setupmetric script

--- wikAPIdia-sr/src/main/scripts/setupmetric.sh
+++ wikAPIdia-sr/src/main/scripts/setupmetric.sh
@@ -1,44 +1,31 @@
 #!/bin/bash
 
-localmetrics=
-universalmetrics=
-case "$1" in
-    all)
-        localmetrics=("ESA" "LocalMilneWitten")
-        universalmetrics=("UniversalMilneWitten")
-        ;;
-    ESA)
-        localmetrics=("ESA")
-        ;;
-    LocalMilneWitten)
-        localmetrics=("LocalMilneWitten")
-        ;;
-    UniversalMilneWitten)
-        universalmetrics=("UniversalMilneWitten")
-        ;;
-    *)
-        echo "unknown metric: $1"
-        exit 1
-esac
+localmetrics=("ESA" "LocalMilneWitten")
+universalmetrics=("UniversalMilneWitten")
 
-shift
 
 source ../wikAPIdia-utils/src/main/scripts/utils.sh &&
 mvn clean &&
 compile || die "$0 failed"
+if [ "$1" == "all" ]; then
+    shift
+    for m in "${localmetrics[@]}"
+    do
+        (echo $m) &&
+        (execClass -Xmx6g org.wikapidia.sr.MatrixBuilder -m $m $@) &&
+        (execClass org.wikapidia.sr.MetricTrainer -m $m $@) ||
+            die "$0 failed"
+    done
 
-for m in "${localmetrics[@]}"
-do
-    (echo $m) &&
-    (execClass -Xmx6g org.wikapidia.sr.MatrixBuilder -m $m $@) &&
-    (execClass org.wikapidia.sr.MetricTrainer -m $m $@) ||
+    for u in "${universalmetrics[@]}"
+    do
+        (echo $m) &&
+        (execClass -Xmx6g org.wikapidia.sr.MatrixBuilder -u $u $@) &&
+        (execClass org.wikapidia.sr.MetricTrainer -u $u $@) ||
+            die "$0 failed"
+    done
+else
+    (execClass -Xmx6g org.wikapidia.sr.MatrixBuilder $@) &&
+    (execClass org.wikapidia.sr.MetricTrainer $@) ||
         die "$0 failed"
-done
-
-for u in "${universalmetrics[@]}"
-do
-    (echo $m) &&
-    (execClass -Xmx6g org.wikapidia.sr.MatrixBuilder -u $u $@) &&
-    (execClass org.wikapidia.sr.MetricTrainer -u $u $@) ||
-        die "$0 failed"
-done
+fi
