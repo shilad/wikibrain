069afc9825406119545f38492f668b42c8b2757f
tobyli <tobyljj@gmail.com>
2013-10-27 13:39:45 -0500

    comment for GetTextByURL

--- wikAPIdia-core/src/main/java/org/wikapidia/core/dao/remote/GetTextByURL.java
+++ wikAPIdia-core/src/main/java/org/wikapidia/core/dao/remote/GetTextByURL.java
@@ -10,7 +10,25 @@ import java.io.*;
  * Time: 7:50 PM
  * To change this template use File | Settings | File Templates.
  */
+
+
+
+
+
+
 public class GetTextByURL {
+
+    /**
+     *
+     * Get the text from a url
+     * @param url the text's location
+     * @return the text
+     * @throws Exception if there was an error retrieving the text
+     *
+     *
+     */
+
+
     public static String getText(String url) throws Exception {
         URL website = new URL(url);
         URLConnection connection = website.openConnection();
