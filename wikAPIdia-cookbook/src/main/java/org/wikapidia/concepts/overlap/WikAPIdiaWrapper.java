e6b53881d18d7aca5b1b0eda85248503f2a7c729
shilad <shilad@gmail.com>
2013-10-29 09:58:23 -0500

    Moving towards replacing semantic similarity service.

--- wikAPIdia-cookbook/src/main/java/org/wikapidia/concepts/overlap/WikAPIdiaWrapper.java
+++ wikAPIdia-cookbook/src/main/java/org/wikapidia/concepts/overlap/WikAPIdiaWrapper.java
@@ -11,7 +11,10 @@ import org.wikapidia.core.lang.LocalId;
 import org.wikapidia.core.model.LocalPage;
 import org.wikapidia.core.model.NameSpace;
 import org.wikapidia.core.model.UniversalPage;
+import org.wikapidia.dao.load.PipelineLoader;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,6 +29,8 @@ import java.util.List;
 public class WikAPIdiaWrapper {
     private int CONCEPT_ALGORITHM_ID = 1;
 
+    public static String BASE_DIR = ".";
+
     private final Env env;
     private LocalPageDao lpDao;
     private LocalLinkDao llDao;
@@ -34,10 +39,10 @@ public class WikAPIdiaWrapper {
     /**
      * Creates a new wrapper object with default configuration settings.
      *
-     * @throws ConfigurationException
+     * @throws org.wikapidia.conf.ConfigurationException
      */
-    public WikAPIdiaWrapper() throws ConfigurationException {
-        this.env = new EnvBuilder().build();
+    public WikAPIdiaWrapper(String baseDir) throws ConfigurationException {
+        env = new EnvBuilder().setBaseDir(new File(baseDir)).build();
         this.lpDao = env.getConfigurator().get(LocalPageDao.class);
         this.llDao = env.getConfigurator().get(LocalLinkDao.class);
         this.upDao = env.getConfigurator().get(UniversalPageDao.class);
@@ -55,7 +60,7 @@ public class WikAPIdiaWrapper {
      * Returns the number of WikiLinks to a particular page.
      * @param page
      * @return
-     * @throws DaoException
+     * @throws org.wikapidia.core.dao.DaoException
      */
     public int getNumInLinks(LocalPage page) throws DaoException {
         DaoFilter filter = new DaoFilter()
@@ -68,7 +73,7 @@ public class WikAPIdiaWrapper {
      * Returns a list of ALL the local pages in a particular language.
      * @param language
      * @return
-     * @throws DaoException
+     * @throws org.wikapidia.core.dao.DaoException
      */
     public List<LocalPage> getLocalPages(Language language) throws DaoException {
         DaoFilter df = new DaoFilter()
@@ -83,7 +88,7 @@ public class WikAPIdiaWrapper {
      * Returns a list of the pages that represent the same concept in other languages.
      * @param page
      * @return
-     * @throws DaoException
+     * @throws org.wikapidia.core.dao.DaoException
      */
     public List<LocalPage> getInOtherLanguages(LocalPage page) throws DaoException {
         int conceptId = upDao.getUnivPageId(page, CONCEPT_ALGORITHM_ID);
@@ -101,5 +106,14 @@ public class WikAPIdiaWrapper {
             }
         }
         return results;
+    }
+
+    /**
+     * Load a set of languages into the h2 database.
+     * THIS MUST BE CALLED BEFORE AN INSTANCE OF WIKIPADIA WRAPPER IS CREATED!
+     * @param langCodes comma separated list of langcodes - ie "simple,la"
+     */
+    public static void loadLanguages(String langCodes) throws IOException, InterruptedException {
+        PipelineLoader.main(new String[]{"-l", langCodes});
     }
 }
