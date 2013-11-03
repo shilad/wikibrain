f94102a5082ed70c71c7e9ba3569a90033879e91
tobyli <tobyljj@gmail.com>
2013-11-03 15:22:45 -0600

    Test Example for new Daos

---
+++ wikAPIdia-cookbook/src/main/java/org/wikapidia/core/cookbook/LiveDaosExample.java
@@ -0,0 +1,72 @@
+package org.wikapidia.core.cookbook;
+
+
+import org.wikapidia.conf.Configuration;
+import org.wikapidia.conf.ConfigurationException;
+import org.wikapidia.conf.Configurator;
+import org.wikapidia.core.dao.*;
+import org.wikapidia.core.lang.Language;
+import org.wikapidia.core.model.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import org.wikapidia.core.dao.live.*;
+/**
+ * Created with IntelliJ IDEA.
+ * User: Toby "Jiajun" Li
+ * Date: 11/3/13
+ * Time: 12:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LiveDaosExample {
+    public static void main(String args[]) throws ConfigurationException, DaoException, IOException {
+        LocalPageDao localPageDao = new Configurator(new Configuration()).get(LocalPageDao.class, "live");
+        LocalArticleDao localArticleDao = new Configurator(new Configuration()).get(LocalArticleDao.class, "live");
+        LocalCategoryDao localCategoryDao = new Configurator(new Configuration()).get(LocalCategoryDao.class, "live");
+        LocalCategoryMemberDao localCategoryMemberDao = new Configurator(new Configuration()).get(LocalCategoryMemberDao.class, "live");
+        Language lang = Language.getByLangCode("en");
+
+
+        System.out.println("\n\nTest for LocalArticleDao and LocalCategoryDao \n");
+        //Test for LocalArticleDao and LocalCategoryDao
+        System.out.println(localArticleDao.getByTitle(lang, new Title("Minnesota", lang)));
+        System.out.println(localCategoryDao.getByTitle(lang, new Title("Category:States of the United States", lang)));
+
+        System.out.println("\n\nTest for LocalArticleDao.getByTitles \n");
+        //Test for LocalArticleDao.getByTitles
+        List<Title> titleList = new ArrayList<Title>();
+        titleList.add(new Title("New York", lang));
+        titleList.add(new Title("Hawaii", lang));
+        Map<Title, LocalArticle> articleMap = localArticleDao.getByTitles(lang, titleList);
+        for(Map.Entry<Title, LocalArticle> e : articleMap.entrySet()){
+            System.out.println(e.getKey() + "    " + e.getValue());
+        }
+        System.out.println("\n\nTest for LocalCategoryDao.getByTitles \n\n");
+        //Test for LocalCategoryDao.getByTitles
+        titleList = new ArrayList<Title>();
+        titleList.add(new Title("Category:Midwestern United States", lang));
+        titleList.add(new Title("Category:Western United States", lang));
+        Map<Title, LocalCategory> localCategoryMap = localCategoryDao.getByTitles(lang, titleList);
+        for(Map.Entry<Title, LocalCategory> e : localCategoryMap.entrySet()){
+            System.out.println(e.getKey() + "    " + e.getValue());
+        }
+
+        System.out.println("\n\nLocalCategoryMemberDao.getCategoryMemberIds (Get all category members of \"Category:Minnesota\" \n");
+        //Test for LocalCategoryMemberDao.getCategoryMemberIds
+        Collection<Integer> memberList = localCategoryMemberDao.getCategoryMemberIds(lang, 704819);    //Id for Category:Minnesota
+        for(Integer e: memberList){
+            System.out.println(localArticleDao.getById(lang, e));
+        }
+
+        System.out.println("\n\nLocalCategoryMemberDao.getCategoryIds (Get all categories of \"Minnesota\" \n");
+        //Test for LocalCategoryMemberDao.getCategoryIds
+        Collection<Integer> categoryList = localCategoryMemberDao.getCategoryIds(lang, 19590);  //Id for Minnesota
+        for(Integer e: categoryList){
+            System.out.println(localCategoryDao.getById(lang, e));
+        }
+
+    }
+}
