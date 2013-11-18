3eebbb29a1b37ad4c41b5a87e8ae8e738404e4bb
Derian Andersen <and01419@umn.edu>
2013-11-18 02:02:36 -0600

    start url integration

--- wikAPIdia-core/src/main/java/org/wikapidia/core/dao/live/LiveAPIQuery.java
+++ wikAPIdia-core/src/main/java/org/wikapidia/core/dao/live/LiveAPIQuery.java
@@ -28,18 +28,123 @@ import java.util.Set;
  */
 public class LiveAPIQuery {
 
-    private final String queryParams; //specify what information should be retrieved via the wiki API
     private final Language lang;
     private final String outputFormat = "json"; //only JSON currently supported
+    private final String queryAction;
+    private final String queryType;
+    private final String queryPagePrefix;
+    private final String queryPrefix;
+    private final Boolean pluralPage;
     private final String queryResultDataSection; //section of the query result containing the data of interest
+    private final QueryParser parser;
     private final boolean parseArray; //if true, the queryResultDataSection is parsed as a JSON array; parsed as a JSON object if false
+    private Boolean redirects;
+    private String title;
+    private Integer pageid;
+    private String filterredir;
+    private String from;
+
+    private String queryUrl;
     private String queryResult = ""; //text representing the raw output of the query
 
     private LiveAPIQuery(LiveAPIQueryBuilder builder) {
-        this.queryParams = builder.queryParams;
         this.lang = builder.lang;
-        this.queryResultDataSection = builder.queryResultDataSection;
-        this.parseArray = builder.parseArray;
+        if (builder.redirects != null) {
+            this.redirects = builder.redirects;
+        }
+        if (builder.title != null) {
+            this.title = builder.title;
+        }
+        if (builder.pageid != null) {
+            this.pageid = builder.pageid;
+        }
+        if (builder.filterredir != null) {
+            this.filterredir = builder.filterredir;
+        }
+        if (builder.from != null) {
+            this.from = builder.from;
+        }
+
+        switch (builder.queryType) {
+            case INFO:
+                this.queryAction = "prop";
+                this.queryType = "info";
+                this.queryPagePrefix = "";
+                this.queryPrefix = "in";
+                this.pluralPage = true;
+                this.queryResultDataSection = "pages";
+                this.parser = new ObjectQueryParser();
+                break;
+            case CATEGORYMEMBERS:
+                this.queryAction = "list";
+                this.queryType = "categorymembers";
+                this.queryPagePrefix = "cm";
+                this.queryPrefix = "cm";
+                this.pluralPage = false;
+                this.queryResultDataSection = "categorymembers";
+                this.parser = new ArrayQueryParser();
+                break;
+            case CATEGORIES:
+                this.queryAction = "prop";
+                this.queryType = "categories";
+                this.queryPagePrefix = "";
+                this.queryPrefix = "cl";
+                this.pluralPage = true;
+                this.queryResultDataSection = "pages";
+                this.parser = new ObjectQueryParser();
+                break;
+            case LINKS:
+                this.queryAction = "generator";
+                this.queryType = "links";
+                this.queryPagePrefix = "";
+                this.queryPrefix = "gpl";
+                this.pluralPage = true;
+                this.queryResultDataSection = "pages";
+                this.parser = new ObjectQueryParser();
+                break;
+            case BACKLINKS:
+                this.queryAction = "list";
+                this.queryType = "backlinks";
+                this.queryPagePrefix = "bl";
+                this.queryPrefix = "bl";
+                this.pluralPage = false;
+                this.queryResultDataSection = "backlinks";
+                this.parser = new ArrayQueryParser();
+                break;
+            default:    //allpages
+                this.queryAction = "list";
+                this.queryType = "allpages";
+                this.queryPagePrefix = "ap";
+                this.queryPrefix = "ap";
+                this.pluralPage = false;
+                this.queryResultDataSection = "allpages";
+                this.parser = new ArrayQueryParser();
+                break;
+        }
+        constructQueryUrl();
+    }
+
+    public void constructQueryUrl() {
+        String http = "http://";
+        String host = ".wikipedia.org";
+        String queryUrl = http + lang.getLangCode() + host + "/w/api.php?action=query&format=" + outputFormat +
+                "&" + queryAction + "=" + queryType + "&" + queryPagePrefix + "limit=500";
+        if (this.title != null) {
+            queryUrl += "&" + queryPagePrefix + "title" + (pluralPage ? "s" : "") + "=" + title;
+        }
+        if (this.pageid != null) {
+            queryUrl += "&" + queryPagePrefix + "pageid" + (pluralPage ? "s" : "") + "=" + pageid;
+        }
+        if (this.redirects != null) {
+            queryUrl += "&redirects=";
+        }
+        if (this.filterredir != null) {
+            queryUrl += "&" + queryPagePrefix + "filterredir" + (pluralPage ? "s" : "") + "=" + filterredir;
+        }
+        if (this.from != null) {
+            queryUrl += "&" + queryPagePrefix + "from" + (pluralPage ? "s" : "") + "=" + from;
+        }
+        this.queryUrl = queryUrl;
     }
 
     /**
@@ -93,14 +198,11 @@ public class LiveAPIQuery {
      * @throws org.wikapidia.core.dao.DaoException
      */
     private void getRawQueryText() throws DaoException {
-        String http = "http://";
-        String host = ".wikipedia.org";
-        String query = http + lang.getLangCode() + host + "/w/api.php?action=query&format=" + outputFormat + "&" + queryParams;
         String info = new String();
         InputStream inputStr;
 
         try{
-            inputStr = new URL(query).openStream();
+            inputStr = new URL(queryUrl).openStream();
             try {
                 info = IOUtils.toString(inputStr);
             }
@@ -212,16 +314,42 @@ public class LiveAPIQuery {
 
     //This class uses the builder method in anticipation of increased complexity over time
     public static class LiveAPIQueryBuilder {
-        private final String queryParams;
         private final Language lang;
-        private final String queryResultDataSection;
-        private final boolean parseArray;
+        private final QueryType queryType;
+        private Boolean redirects;
+        private String title;
+        private Integer pageid;
+        private String filterredir;
+        private String from;
 
-        public LiveAPIQueryBuilder(String queryParams, Language lang, String queryResultDataSection, boolean parseArray) {
-            this.queryParams = queryParams;
+        public LiveAPIQueryBuilder(QueryType queryType, Language lang) {
+            this.queryType = queryType;
             this.lang = lang;
-            this.queryResultDataSection = queryResultDataSection;
-            this.parseArray = parseArray;
+        }
+
+        public LiveAPIQueryBuilder setRedirects(Boolean redirects) {
+            this.redirects = redirects;
+            return this;
+        }
+
+        public LiveAPIQueryBuilder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public LiveAPIQueryBuilder setPageid(Integer pageid) {
+            this.pageid = pageid;
+            return this;
+        }
+
+        public LiveAPIQueryBuilder setFilterredir(String filterredir) {
+            this.filterredir = filterredir;
+            return this;
+        }
+
+        public LiveAPIQueryBuilder setFrom(String from) {
+            this.from = from;
+            return this;
         }
 
         public LiveAPIQuery build() {
