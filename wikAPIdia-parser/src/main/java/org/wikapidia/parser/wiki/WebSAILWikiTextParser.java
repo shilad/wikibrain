b4272604674caf314764199593398a70c8ec5392
Thanapon Noraset <flcl.tvboy@gmail.com>
2013-11-18 18:27:01 -0600

    Add a dependency for an external parser and initial commit for WebSAILWikiTextParser

---
+++ wikAPIdia-parser/src/main/java/org/wikapidia/parser/wiki/WebSAILWikiTextParser.java
@@ -0,0 +1,17 @@
+package org.wikapidia.parser.wiki;
+
+import java.util.List;
+
+import org.wikapidia.core.WikapidiaException;
+import org.wikapidia.core.model.RawPage;
+
+public class WebSAILWikiTextParser implements WikiTextParser {
+
+	@Override
+	public void parse(RawPage xml, List<ParserVisitor> visitors)
+			throws WikapidiaException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
