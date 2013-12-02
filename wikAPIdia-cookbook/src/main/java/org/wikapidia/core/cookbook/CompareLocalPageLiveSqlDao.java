c51643a19fdbd3a98d33bbed3513de7e86f18bbb
tobyli <tobyljj@gmail.com>
2013-12-02 08:32:16 -0600

    LiveDao cookbook examples with CSV output

--- wikAPIdia-cookbook/src/main/java/org/wikapidia/core/cookbook/CompareLocalPageLiveSqlDao.java
+++ wikAPIdia-cookbook/src/main/java/org/wikapidia/core/cookbook/CompareLocalPageLiveSqlDao.java
@@ -7,7 +7,10 @@ import org.wikapidia.core.dao.DaoException;
 import org.wikapidia.core.dao.LocalPageDao;
 import org.wikapidia.core.lang.Language;
 import org.wikapidia.core.model.LocalPage;
+import au.com.bytecode.opencsv.CSVWriter;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 /**
@@ -20,9 +23,12 @@ public class CompareLocalPageLiveSqlDao  {
         LocalPageDao localPageSqlDao = new Configurator(new Configuration()).get(LocalPageDao.class, "sql");
         Language lang = Language.getByLangCode("simple");
         LocalPage livePage, sqlPage;
+        File f=new File("../wikAPIdia-cookbook/pagestat.csv");
+        String[] entries = new String[3];
+        CSVWriter csvWriter = new CSVWriter(new FileWriter(f), ',');
 
         int liveCount = 0, sqlCount = 0, commonCount = 0;
-        for(int i = 0; i < 200; i++){
+        for(int i = 0; i < 30; i++){
             int id = (int)(Math.random() * 500000);
             boolean flag = false;
             try {
@@ -36,8 +42,9 @@ public class CompareLocalPageLiveSqlDao  {
                 try {
                     sqlPage = localPageSqlDao.getById(lang, id);
                     if(sqlPage != null){
-                        System.out.printf("LiveDao Failed to get page ");
-                        System.out.println(sqlPage);
+                        entries[0] = "LiveDao Failed to get page ";
+                        entries[1] = sqlPage.toString();
+                        csvWriter.writeNext(entries);
                     }
                 }
                 catch (Exception e2){}
@@ -56,15 +63,21 @@ public class CompareLocalPageLiveSqlDao  {
                 try {
                     livePage = localPageLiveDao.getById(lang, id);
                     if(livePage != null){
-                        System.out.printf("SqlDao Failed to get page ");
-                        System.out.println(livePage);
+                        entries[0] = "SQLDao Failed to get page ";
+                        entries[1] = livePage.toString();
+                        csvWriter.writeNext(entries);
                     }
                 }
                 catch (Exception e2){}
             }
 
         }
+        System.out.println("Detailed error information is printed to pagestat.csv at wikAPIdia-cookbook directory");
         System.out.printf("Successfully get live page: %d\nSuccessfully get sql page: %d\nSuccessfully get both page: %d\n", liveCount, sqlCount, commonCount);
+        entries = String.format("Successfully get live page: %d#Successfully get sql page: %d#Successfully get both page: %d#", liveCount, sqlCount, commonCount).split("#");
+        csvWriter.writeNext(entries);
+        csvWriter.close();
     }
+
 
 }
