924c42d69ba5afe9cc89bade72ef7652a7476c9f
Toby Li <li@toby.li>
2014-02-04 01:54:16 -0600

    New cookbook example of 6-degree-separation

---
+++ wikAPIdia-cookbook/src/main/java/org/wikapidia/core/cookbook/ConceptRelationTest.java
@@ -0,0 +1,35 @@
+package org.wikapidia.core.cookbook;
+
+import org.wikapidia.conf.Configuration;
+import org.wikapidia.conf.ConfigurationException;
+import org.wikapidia.conf.Configurator;
+import org.wikapidia.core.cmd.Env;
+import org.wikapidia.core.cmd.EnvBuilder;
+import org.wikapidia.core.dao.DaoException;
+import org.wikapidia.core.dao.LocalPageDao;
+import org.wikapidia.core.dao.RedirectDao;
+import org.wikapidia.core.lang.Language;
+import org.wikapidia.core.model.LocalPage;
+import java.io.IOException;
+import org.wikapidia.core.cookbook.ConceptRelation;
+import org.wikapidia.core.model.NameSpace;
+
+/** An example shows the usage of ConceptRelation class
+ * @author Toby "Jiajun" Li
+ */
+public class ConceptRelationTest{
+
+
+    public static void main(String args[]) throws ConfigurationException, DaoException, IOException {
+        ConceptRelation cr = new ConceptRelation(Language.getByLangCode("simple"));
+
+        System.out.println(cr.getRelationSR("Minnesota", "Algorithm"));
+        System.out.println(cr.getRelationBidirectional("Minnesota", "Peanut milk"));
+        Integer srcId = 1527;
+        Integer dstId = 43788;
+        //System.out.println(cr.getWikidataRelation(srcId, dstId));
+
+    }
+
+
+}
