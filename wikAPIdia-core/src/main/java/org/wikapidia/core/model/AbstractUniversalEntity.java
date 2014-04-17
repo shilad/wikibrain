8bf1686ab591f13325227c2570d9da9a0094d8ce
shilad <shilad@gmail.com>
2014-04-17 09:25:18 -0500

    Optimizations. Why are things slow?

--- wikAPIdia-core/src/main/java/org/wikapidia/core/model/AbstractUniversalEntity.java
+++ wikAPIdia-core/src/main/java/org/wikapidia/core/model/AbstractUniversalEntity.java
@@ -93,7 +93,7 @@ public abstract class AbstractUniversalEntity<T> {
         return true;
     }
 
-    public boolean isInLanguageSet(LanguageSet ls) {
+    public boolean hasAllLanguages(LanguageSet ls) {
         return isInLanguageSet(ls, true);
     }
 
