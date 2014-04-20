645adf610a66d4c03cd80e9d9b02a4311ee75465
aaroniidx <jialun.jiang@gmail.com>
2014-04-19 20:32:02 -0500

    add the new converter method. bugs not fixed yet.

--- wikAPIdia-spatial/src/main/java/org/wikapidia/spatial/cookbook/ShapeFileConverterTest.java
+++ wikAPIdia-spatial/src/main/java/org/wikapidia/spatial/cookbook/ShapeFileConverterTest.java
@@ -7,17 +7,15 @@ import java.net.MalformedURLException;
 
 /**
  * Created by aaroniidx on 4/13/14.
+ * Uses the less big China shape file as an example
  */
 public class ShapeFileConverterTest {
 
     public static void main(String[] args){
         GADMConverter converter = new GADMConverter();
         //converter.downloadGADMShapeFile();
-        try {
-            ShpFiles shpFile = new ShpFiles("tmp/gadm_v2_shp/gadm2.dbf");
-            converter.convertShpFile(shpFile);
-        } catch (MalformedURLException e){
-            e.printStackTrace();
-        }
+
+            converter.convertShpFile("tmp/CHN_adm/CHN_adm2.shp");
+
     }
 }
--- wikAPIdia-spatial/src/main/java/org/wikapidia/spatial/loader/GADMConverter.java
+++ wikAPIdia-spatial/src/main/java/org/wikapidia/spatial/loader/GADMConverter.java
@@ -4,6 +4,7 @@ package org.wikapidia.spatial.loader;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryCollection;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.PrecisionModel;
 import net.lingala.zip4j.core.ZipFile;
@@ -18,6 +19,29 @@ import org.geotools.data.shapefile.dbf.DbaseFileWriter;
 import org.geotools.data.shapefile.files.ShpFiles;
 import org.geotools.data.shapefile.shp.ShapefileException;
 import org.geotools.data.shapefile.shp.ShapefileReader;
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.feature.SchemaException;
+import org.geotools.geometry.jts.*;
+import org.geotools.data.DataUtilities;
+import org.geotools.data.DefaultTransaction;
+import org.geotools.data.Transaction;
+import org.geotools.data.collection.ListFeatureCollection;
+import org.geotools.data.shapefile.ShapefileDataStore;
+import org.geotools.data.shapefile.ShapefileDataStoreFactory;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.data.simple.SimpleFeatureStore;
+import org.geotools.data.*;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.geotools.geometry.jts.JTSFactoryFinder;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.opengis.geometry.*;
+import org.opengis.geometry.coordinate.*;
+import org.geotools.geometry.jts.*;
 import org.hibernate.annotations.SourceType;
 import org.wikapidia.conf.ConfigurationException;
 import org.wikapidia.conf.Configurator;
@@ -33,12 +57,15 @@ import org.wikapidia.spatial.core.dao.postgis.PostGISDB;
 import org.wikapidia.wikidata.WikidataDao;
 import net.lingala.zip4j.*;
 
+import java.awt.*;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.channels.FileChannel;
 import java.nio.channels.WritableByteChannel;
 import java.nio.charset.Charset;
 import java.util.*;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -50,6 +77,7 @@ public class GADMConverter {
 
     /**
      * Download GADM shape file
+     *
      */
     public void downloadGADMShapeFile() {
 
@@ -75,13 +103,126 @@ public class GADMConverter {
         }
     }
 
-    //TODO: fix the index out of bound bug
+
+    /**
+     *
+     * @param fileName
+     * Takes in the GADM shape file and convert it into the kind of shape file that we can read
+     * For some reason it won't recognize the CRS ID
+     */
+    public void convertShpFile(String fileName) {
+        File file = new File(fileName);
+        Map map = new HashMap();
+        HashMap<String, List<Geometry>> stateShape = new HashMap<>();
+        HashMap<String, String> stateCountry = new HashMap<>();
+        File outputFile = new File("gadm2.shp");
+
+        ShapefileReader shpReader;
+        GeometryFactory geometryFactory;
+        SimpleFeatureBuilder featureBuilder;
+        DataStore inputDataStore;
+        List<SimpleFeature> features = new ArrayList<SimpleFeature>();
+
+        try{
+            map.put("url", file.toURI().toURL());
+            inputDataStore = DataStoreFinder.getDataStore(map);
+            SimpleFeatureSource inputFeatureSource = inputDataStore.getFeatureSource(inputDataStore.getTypeNames()[0]);
+            SimpleFeatureCollection inputCollection = inputFeatureSource.getFeatures();
+            SimpleFeatureIterator inputFeatures = inputCollection.features();
+            while (inputFeatures.hasNext()) {
+                SimpleFeature feature = inputFeatures.next();
+                //feature.getType().getGeometryDescriptor().getType().getBinding();
+
+                if (!stateShape.containsKey(feature.getAttribute(5))) {
+                    stateShape.put((String) feature.getAttribute(5), new ArrayList<Geometry>());
+                    stateCountry.put((String) feature.getAttribute(5), (String) feature.getAttribute(3)); //set up the state-country map
+                }
+
+                stateShape.get(feature.getAttribute(5)).add((Geometry)feature.getAttribute(0)); //and put all the polygons under a state into another map
+            }
+
+
+
+            final SimpleFeatureType WIKITYPE = DataUtilities.createType("wikiType",
+                    "geom:MultiPolygon:srid=4326, TITLE1_EN:String, TITLE2_EN:String"  // Code 4326 not found
+            );
+            geometryFactory = JTSFactoryFinder.getGeometryFactory();
+            featureBuilder = new SimpleFeatureBuilder(WIKITYPE);
+
+            System.out.println("Processing polygons...");
+
+            for (String state: stateCountry.keySet()){    //create the feature collection for the new shpfile
+                Geometry newGeom = geometryFactory.buildGeometry(stateShape.get(state)).buffer(0);
+                featureBuilder.add(newGeom);
+                featureBuilder.add(state);
+                featureBuilder.add(state + ", " + stateCountry.get(state));
+                SimpleFeature feature = featureBuilder.buildFeature(null);
+                features.add(feature);
+            }
+
+            System.out.println("Processing complete.");
+
+            ShapefileDataStoreFactory dataStoreFactory = new ShapefileDataStoreFactory();  //create the output datastore
+
+            Map<String, Serializable> outputParams = new HashMap<String, Serializable>();
+            outputParams.put("url", outputFile.toURI().toURL());
+            outputParams.put("create spatial index", Boolean.TRUE);
+
+            ShapefileDataStore outputDataStore = (ShapefileDataStore) dataStoreFactory.createNewDataStore(outputParams);
+
+            outputDataStore.createSchema(WIKITYPE);
+
+            Transaction transaction = new DefaultTransaction("create");
+
+            String typeName = outputDataStore.getTypeNames()[0];
+            SimpleFeatureSource outputFeatureSource = outputDataStore.getFeatureSource(typeName);
+            SimpleFeatureType SHAPE_TYPE = outputFeatureSource.getSchema();
+
+            if (outputFeatureSource instanceof SimpleFeatureStore) {
+                SimpleFeatureStore featureStore = (SimpleFeatureStore) outputFeatureSource;
+
+                SimpleFeatureCollection collection = new ListFeatureCollection(WIKITYPE, features);
+                featureStore.setTransaction(transaction);
+                try {
+                    featureStore.addFeatures(collection);
+                    transaction.commit();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    transaction.rollback();
+                } finally {
+                    transaction.close();
+                }
+                System.exit(0); // success!
+            } else {
+                System.out.println(typeName + " does not support read/write access");
+                System.exit(1);
+            }
+
+
+        } catch (SchemaException e){
+            e.printStackTrace();
+
+        } catch (MalformedURLException e){
+            e.printStackTrace();
+
+        } catch (IOException e){
+            e.printStackTrace();
+
+        }
+
+
+
+    }
+
+
 
     /**
      * Convert GADM shapefile into the format we can read
      * @param shpFile
      */
-    public void convertShpFile(ShpFiles shpFile) {
+
+    @Deprecated
+    private void convertShpFile(ShpFiles shpFile) {
         DbaseFileReader dbfReader;
         DbaseFileWriter dbfWriter;
         DbaseFileHeader dbfHeader;
@@ -116,6 +257,7 @@ public class GADMConverter {
         }
 
     }
+
 
 
 
