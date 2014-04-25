b51eeb16dcd56f7dc219971987781b73847d70a4
tobyli <li@toby.li>
2014-04-24 21:35:50 -0500

    getMaxDistanceKmItemIds

--- wikibrain-spatial/src/main/java/org/wikapidia/spatial/core/dao/SpatialNeighborDao.java
+++ wikibrain-spatial/src/main/java/org/wikapidia/spatial/core/dao/SpatialNeighborDao.java
@@ -12,17 +12,48 @@ import java.util.Set;
 public interface SpatialNeighborDao {
     /**
      * Returns the item ids of the items spatially located with a given distance range with the geometry corresponding to the input (itemId, layerName, refSysName).
-     * @param itemId itemId of the container
-     * @param layerName layer of the container
-     * @param refSysName refSysName of the container
-     * @param subLayers the layers in which to search for contained objects (in the input ref sys)
-     * @param minDist the min distance range
-     * @param maxDist the max distance range
-     * @return
+     * @param itemId itemId of the starting point
+     * @param layerName layer of the starting point
+     * @param refSysName refSysName of the starting point
+     * @param subLayers the layers in which to search for the objects (in the input ref sys)
+     * @param minDist the min distance range in angular distance
+     * @param maxDist the max distance range in angular distance
+     * @return a IntSet contatins spatial items with in the given range in the given layer
      * @throws org.wikapidia.core.dao.DaoException
      */
     public TIntSet getNeighboringItemIds(Integer itemId, String layerName, String refSysName, Set<String> subLayers, double minDist, double maxDist) throws DaoException;
-
+    /**
+     * Returns the item ids of the items spatially located with a given distance range with the geometry corresponding to the input (itemId, layerName, refSysName).
+     * @param g The starting point
+     * @param refSysName refSysName of the starting point
+     * @param subLayers the layers in which to search for the objects (in the input ref sys)
+     * @param minDist the min distance range in angular
+     * @param maxDist the max distance range in angular
+     * @return a IntSet contatins spatial items with in the given range in the given layer
+     * @throws org.wikapidia.core.dao.DaoException
+     */
     public TIntSet getNeighboringItemIds(Geometry g, String refSysName, Set<String> subLayers, double minDist, double maxDist) throws DaoException;
+    /**
+     * Returns the item ids of the items spatially located with a given distance range with the geometry corresponding to the input (itemId, layerName, refSysName).
+     * @param itemId itemId of the starting point
+     * @param layerName layer of the starting point
+     * @param refSysName refSysName of the starting point
+     * @param subLayers the layers in which to search for the objects (in the input ref sys)
+     * @param maxDist the min distance range in km
+     * @return a IntSet contatins spatial items with in the given range in the given layer
+     * @throws org.wikapidia.core.dao.DaoException
+     */
+    public TIntSet getMaxDistanceKmItemIds(Integer itemId, String layerName, String refSysName, Set<String> subLayers, double maxDist) throws DaoException;
+    /**
+     * Returns the item ids of the items spatially located with a given distance range with the geometry corresponding to the input (itemId, layerName, refSysName).
+     * @param g The starting point
+     * @param refSysName refSysName of the starting point
+     * @param subLayers the layers in which to search for the objects (in the input ref sys)
+     * @param maxDist the min distance range in km
+     * @return a IntSet contatins spatial items with in the given range in the given layer
+     * @throws org.wikapidia.core.dao.DaoException
+     */
+    public TIntSet getMaxDistanceKmItemIds(Geometry g, String refSysName, Set<String> subLayers, double maxDist) throws DaoException;
+
 
 }
