68a909b317cf6463dbbe27383061eb5e97cc92d4
tobyli <li@toby.li>
2014-05-22 16:58:34 -0500

    Use basic loader for Wikidata layer loader

--- wikibrain-spatial/src/main/java/org/wikibrain/spatial/loader/SpatialDataLoader.java
+++ wikibrain-spatial/src/main/java/org/wikibrain/spatial/loader/SpatialDataLoader.java
@@ -193,8 +193,8 @@ public class SpatialDataLoader {
 
             // this should eventually be moved into a config file or parameters of the parse
             List<WikidataLayerLoader> layerLoaders = Lists.newArrayList();
-//            layerLoaders.add(new EarthBasicCoordinatesWikidataLayerLoader(wdDao, spatialDataDao));
-            layerLoaders.add(new EarthInstanceOfCoordinatesLayerLoader(wdDao, spatialDataDao));
+            layerLoaders.add(new EarthBasicCoordinatesWikidataLayerLoader(wdDao, spatialDataDao));
+//            layerLoaders.add(new EarthInstanceOfCoordinatesLayerLoader(wdDao, spatialDataDao));
 
             for (WikidataLayerLoader layerLoader : layerLoaders) {
                 LOG.log(Level.INFO, "Loading Wikidata layer(s): " + layerLoader.getClass().getName());
