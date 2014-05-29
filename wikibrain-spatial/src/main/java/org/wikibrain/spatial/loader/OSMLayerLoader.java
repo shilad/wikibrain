d76f8dc886526fbdb6beb432ea8ecf96e0dcbc99
aaroniidx <jialun.jiang@gmail.com>
2014-05-29 01:27:27 -0500

    finish OSMLayerLoader, untested

--- wikibrain-spatial/src/main/java/org/wikibrain/spatial/loader/OSMLayerLoader.java
+++ wikibrain-spatial/src/main/java/org/wikibrain/spatial/loader/OSMLayerLoader.java
@@ -1,10 +1,41 @@
 package org.wikibrain.spatial.loader;
 
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import org.geotools.data.DefaultTransaction;
+import org.geotools.data.Transaction;
+import org.geotools.data.collection.ListFeatureCollection;
+import org.geotools.data.shapefile.ShapefileDataStore;
+import org.geotools.data.shapefile.ShapefileDataStoreFactory;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.data.simple.SimpleFeatureStore;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.geotools.geojson.geom.GeometryJSON;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
 import org.wikibrain.core.WikiBrainException;
 import org.wikibrain.core.dao.DaoException;
+import org.wikibrain.core.lang.Language;
+
+import org.wikibrain.utils.ParallelForEach;
+import org.wikibrain.utils.Procedure;
 import org.wikibrain.wikidata.WikidataDao;
 import org.wikibrain.wikidata.WikidataFilter;
 import org.wikibrain.wikidata.WikidataStatement;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
@@ -14,7 +45,7 @@ import org.wikibrain.wikidata.WikidataStatement;
  */
 public class OSMLayerLoader {
 
-
+    public static final Logger LOG = Logger.getLogger(GADMConverter.class.getName());
 
     private final WikidataDao wdDao;
 
@@ -26,22 +57,130 @@ public class OSMLayerLoader {
 
     }
 
-    public void printGeometries() throws WikiBrainException{
+    public void printGeometries(File outputFile) throws Exception{
+
+        final SimpleFeatureType OSMTYPE = getOutputFeatureType();
+        final SimpleFeatureSource outputFeatureSource = getOutputDataFeatureSource(outputFile, OSMTYPE);
+        final Transaction transaction = new DefaultTransaction("create");
+        final ConcurrentLinkedQueue<List<SimpleFeature>> writeQueue = new ConcurrentLinkedQueue<List<SimpleFeature>>();
 
         try {
 
             Iterable<WikidataStatement> osmRelations = getAllOSMRelations();
 
-            for (WikidataStatement osmRelation : osmRelations){
-                osmRelation.getValue();
+            ParallelForEach.iterate(osmRelations.iterator(), new Procedure<WikidataStatement>() {
+                @Override
+                public void call(WikidataStatement osmRelation) throws Exception {
 
-                //http://wiki.openstreetmap.org/wiki/API_v0.6#Read:_GET_.2Fapi.2F0.6.2F.5Bnode.7Cway.7Crelation.5D.2F.23id
-            }
+                    int itemId = osmRelation.getItem().getId();
+                    String itemLabel = osmRelation.getItem().getLabels().get(Language.SIMPLE); //language might need to change to EN
+
+                    synchronized (this) {LOG.log(Level.INFO, "Writing " + itemLabel + " to the shapefile.");}
+
+                    Geometry itemGeometry = getGeometry(readGeoJson(itemId));
+                    List<SimpleFeature> features = buildOutputFeature(itemGeometry, itemLabel, OSMTYPE);
+                    writeToShpFile(outputFeatureSource, OSMTYPE, transaction, features);
+
+                }
+            });
+
+            /*for (WikidataStatement osmRelation : osmRelations){
+                int itemId = osmRelation.getItem().getId();
+                String itemLabel = osmRelation.getItem().getLabels().get(Language.SIMPLE); //language might need to change to EN
+
+                Geometry itemGeometry = getGeometry(readGeoJson(itemId));
+                List<SimpleFeature> features = buildOutputFeature(itemGeometry, itemLabel, OSMTYPE);
+                writeToShpFile(outputFeatureSource, OSMTYPE, transaction, features);
+            }*/
 
 
-        }catch(DaoException e){
+        } catch(DaoException e){
             throw new WikiBrainException(e);
+        } finally {
+            transaction.close();
         }
+
+    }
+
+    private synchronized void writeToShpFile(SimpleFeatureSource outputFeatureSource, SimpleFeatureType outputFeatureType, Transaction transaction, List<SimpleFeature> features) throws IOException {
+        if (outputFeatureSource instanceof SimpleFeatureStore) {
+            SimpleFeatureStore featureStore = (SimpleFeatureStore) outputFeatureSource;
+
+            SimpleFeatureCollection collection = new ListFeatureCollection(outputFeatureType, features);
+            featureStore.setTransaction(transaction);
+            try {
+                featureStore.addFeatures(collection);
+                transaction.commit();
+            } catch (Exception e) {
+                e.printStackTrace();
+                transaction.rollback();
+            }
+        } else {
+            LOG.log(Level.INFO, "OSMTYPE does not support read/write access");
+        }
+
+    }
+
+    private List<SimpleFeature> buildOutputFeature(Geometry itemGeom, String itemLabel, SimpleFeatureType outputFeatureType) {
+
+        SimpleFeatureBuilder featureBuilder = new SimpleFeatureBuilder(outputFeatureType);
+        featureBuilder.add(itemGeom);
+        featureBuilder.add(itemLabel);
+
+        SimpleFeature feature = featureBuilder.buildFeature(null);
+
+        List<SimpleFeature> features = new ArrayList<SimpleFeature>();
+        features.add(feature);
+
+        return features;
+
+    }
+
+    private SimpleFeatureSource getOutputDataFeatureSource(File outputFile, SimpleFeatureType type) throws IOException {
+
+        ShapefileDataStoreFactory dataStoreFactory = new ShapefileDataStoreFactory();
+        Map<String, Serializable> outputParams = new HashMap<String, Serializable>();
+        outputParams.put("url", outputFile.toURI().toURL());
+        outputParams.put("create spatial index", Boolean.TRUE);
+
+        ShapefileDataStore outputDataStore = (ShapefileDataStore) dataStoreFactory.createNewDataStore(outputParams);
+        outputDataStore.createSchema(type);
+        String typeName = outputDataStore.getTypeNames()[0];
+        return outputDataStore.getFeatureSource(typeName);
+
+    }
+
+    private Geometry getGeometry(String geoJsonString) throws IOException {
+
+        GeometryJSON geoJson = new GeometryJSON();
+        Reader reader = new StringReader(geoJsonString);
+
+        return geoJson.readGeometryCollection(reader);
+
+    }
+
+    private String readGeoJson(int itemId) throws MalformedURLException, IOException{
+
+        String baseURL = "http://tools.wmflabs.org/wiwosm/osmjson/getGeoJSON.php?lang=wikidata&article=Q";
+        URL jsonURL = new URL(baseURL + itemId);
+        String geoJson;
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(jsonURL.openStream()));
+        geoJson = in.readLine();
+        in.close();
+
+        return geoJson;
+    }
+
+    private SimpleFeatureType getOutputFeatureType() {
+        SimpleFeatureTypeBuilder typeBuilder = new SimpleFeatureTypeBuilder();
+        typeBuilder.setName("OSMTYPE");
+        typeBuilder.setCRS(DefaultGeographicCRS.WGS84);
+        typeBuilder.add("the_geom", MultiPolygon.class);
+        typeBuilder.add("TITLE1_EN", String.class);
+        typeBuilder.setDefaultGeometry("the_geom");
+
+        return typeBuilder.buildFeatureType();
 
     }
 
