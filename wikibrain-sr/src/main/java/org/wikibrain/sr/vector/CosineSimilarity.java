1826d4083759ae82cecf74a698694d6e8847e356
Alan Morales <alanrmorales@gmail.com>
2014-06-07 11:54:52 -0500

    Fix for one of the vectors with length 0 (2). I have run the tests and they show no errors.

--- wikibrain-sr/src/main/java/org/wikibrain/sr/vector/CosineSimilarity.java
+++ wikibrain-sr/src/main/java/org/wikibrain/sr/vector/CosineSimilarity.java
@@ -60,32 +60,34 @@ public class CosineSimilarity implements VectorSimilarity {
         int nb = b.getNumCols();
         int i = 0, j = 0;
 
+        if((na == 0 || nb == 0)) { // do not perform calculations if one or both are 0
+            return 0;
+        }
+
         int ca = a.getColIndex(i);
         int cb = b.getColIndex(j);
 
-        if( (i != 0 || j != 0) ) { // do not perform calculations if one or both are 0
-            while (i < na && j < nb) {
-                if (ca < cb) {
-                    // if matrix a has a lower value, then get the next column
-                    float va = a.getColValue(i++);
-                    adota += va * va;
-                    ca = a.getColIndex(i);
-                } else if (ca > cb) {
-                    // if matrix b has a lower value, then get the next column
-                    float vb = b.getColValue(j++);
-                    bdotb += vb * vb;
-                    cb = b.getColIndex(j);
-                } else {
-                    // if both have the same value, increment the intersection and get the next columns in both matrices
-                    float va = a.getColValue(i++);
-                    float vb = b.getColValue(j++);
-                    adota += va * va;
-                    bdotb += vb * vb;
-                    adotb += va * vb;
+        while (i < na && j < nb) {
+            if (ca < cb) {
+                // if matrix a has a lower value, then get the next column
+                float va = a.getColValue(i++);
+                adota += va * va;
+                ca = a.getColIndex(i);
+            } else if (ca > cb) {
+                // if matrix b has a lower value, then get the next column
+                float vb = b.getColValue(j++);
+                bdotb += vb * vb;
+                cb = b.getColIndex(j);
+            } else {
+                // if both have the same value, increment the intersection and get the next columns in both matrices
+                float va = a.getColValue(i++);
+                float vb = b.getColValue(j++);
+                adota += va * va;
+                bdotb += vb * vb;
+                adotb += va * vb;
 
-                    ca = a.getColIndex(i);
-                    cb = b.getColIndex(j);
-                }
+                ca = a.getColIndex(i);
+                cb = b.getColIndex(j);
             }
         }
 
