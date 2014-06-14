692788f5a8dda4516cf2f8762d9e8814dd3d7a86
shilad <shilad@gmail.com>
2014-06-13 22:19:19 -0500

    Initial implementation of the AtomicByteArray

--- wikibrain-utils/src/main/java/org/wikibrain/utils/AtomicByteArray.java
+++ wikibrain-utils/src/main/java/org/wikibrain/utils/AtomicByteArray.java
@@ -3,22 +3,38 @@ package org.wikibrain.utils;
 import java.util.concurrent.atomic.AtomicIntegerArray;
 
 /**
+ * An adaptation of Java's AtomicIntegerArray for bytes.
+ *
+ * Four bytes are packed into each int. Only a few methods are implemented for now.
+ *
  * @author Shilad Sen
  */
 public class AtomicByteArray {
     private final AtomicIntegerArray array;
     private final int length;
 
+    /**
+     * Creates a new AtomicByteArray of the given length, with all
+     * elements initially zero.
+     *
+     * @param length the length of the array
+     */
     public AtomicByteArray(final int length) {
         this.length = length;
         this.array = new AtomicIntegerArray((length + 3) / 4);
     }
 
-    public void set(final int i, byte val) {
+    /**
+     * Sets the element at position {@code i} to the given value.
+     *
+     * @param i the index
+     * @param newValue the new value
+     */
+    public void set(int i, byte newValue) {
         int idx = i >>> 2;
         int shift = (i & 3) << 3;
         int mask = 0xFF << shift;
-        int val2 = (val & 0xff) << shift;
+        int val2 = (newValue & 0xff) << shift;
 
         while (true) {
             final int num = this.array.get(idx);
@@ -29,12 +45,22 @@ public class AtomicByteArray {
         }
     }
 
-    public boolean compareAndSet(final int i, final byte expected, final byte val) {
+    /**
+     * Atomically sets the element at position {@code i} to the given
+     * updated value if the current value {@code ==} the expected value.
+     *
+     * @param i the index
+     * @param expect the expected value
+     * @param update the new value
+     * @return true if successful. False return indicates that
+     * the actual value was not equal to the expected value.
+     */
+    public boolean compareAndSet(int i, byte expect, byte update) {
         int idx = i >>> 2;
         int shift = (i & 3) << 3;
         int mask = 0xFF << shift;
-        int expected2 = (expected & 0xff) << shift;
-        int val2 = (val & 0xff) << shift;
+        int expected2 = (expect & 0xff) << shift;
+        int val2 = (update & 0xff) << shift;
 
         while (true) {
             final int num = this.array.get(idx);
@@ -49,10 +75,21 @@ public class AtomicByteArray {
         }
     }
 
+    /**
+     * Gets the current value at position {@code i}.
+     *
+     * @param i the index
+     * @return the current value
+     */
     public byte get(final int i) {
         return (byte) (this.array.get(i >>> 2) >> ((i & 3) << 3));
     }
 
+    /**
+     * Returns the length of the array.
+     *
+     * @return the length of the array
+     */
     public int length() {
         return this.length;
     }
