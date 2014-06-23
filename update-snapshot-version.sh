1a4df38fe58c907059ec28cf76bf8a6462a8c563
shilad <shilad@gmail.com>
2014-06-22 23:41:51 -0500

    Merge branch 'gh-pages' of https://github.com/shilad/wikibrain into gh-pages
    Conflicts:
    	_config.yml

--- update-snapshot-version.sh
+++ update-snapshot-version.sh
@@ -6,27 +6,21 @@ if [ -z "$1" ]; then
 fi
 
 new_version=$1
-pattern="([0-9+]).([0-9])+.([0-9])+"
+pattern="([0-9+]).([0-9])+-SNAPSHOT"
 
 if ! [[ "$new_version" =~ $pattern ]]; then
-    echo "version must be 0.majornum.minornum" >&2
+    echo "version must be 0.majornum-SNAPSHOT" >&2
     exit 1
 fi
 
 version1=${BASH_REMATCH[1]}
 version2=${BASH_REMATCH[2]}
-version3=${BASH_REMATCH[3]}
-branch=release-${version1}.${version2}
 
-ALL_JARS=`pwd`/.tmp/all-jars
 
 old_version=$(grep version ./pom.xml | tail -1 | sed 's/.*<version>//' | sed 's/<\/version>//')
-target=`pwd`/wikibrain-and-deps-${new_version}.zip
 
 echo "current version is $old_version"
 echo "new version will be $new_version"
-echo "new branch will be $branch"
-echo "new zipfile will be $target"
 echo "if all these things seem correct, type 'go'"
 
 read GO
@@ -35,29 +29,16 @@ if [ ${GO} != "go" ]; then
     echo "aborting!"
 fi
 
-rm -rf ${ALL_JARS} ${target}
-
 # prepare release
-git branch ${branch} &&
+git checkout master &&
 mvn clean &&
 mvn versions:set -DnewVersion=${new_version} &&
 mvn  -f ./wikibrain-parent/pom.xml versions:set -DnewVersion=${new_version} &&
 mvn  -N versions:update-child-modules &&
 mvn versions:commit ||
-{ echo "updating versions failed" >&2; exit 1 }
+{ echo "updating versions failed" >&2; exit 1; }
 
-# push release and commit
+# git commit
 mvn clean compile package &&
-git commit -m "Released ${new_version}" &&
-mvn clean deploy -P release-version -DskipTests=true &&
-mvn nexus-staging:release  -P release-version -DskipTests=true ||
-{ echo "Pushing release to git or sonatype failed" >&2; exit 1 }
-
-# zip up full jar
-mvn dependency:copy-dependencies -DoutputDirectory=${ALL_JARS} &&
-cp -p wikibrain*/target/wikibrain*${new_version}.jar ${ALL_JARS} &&
-(cd ${ALL_JARS} && zip ${target} *.jar) &&
-scp -p ${target} shilad.com:/var/www/html/www.shilad.com/wikibrain ||
-{ echo "Uploading master zip failed" >&2; exit 1 }
-
-
+git commit -m "Bumped snapshot to ${new_version}" ||
+{ echo "Pushing release to git or sonatype failed" >&2; exit 1; }
