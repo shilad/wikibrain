a9cf034a5269efec1f74a36c66e294f0e0929f30
shilad <shilad@gmail.com>
2014-06-22 22:33:55 -0500

    Preparing for first official release.

--- push-major-release.sh
+++ push-major-release.sh
@@ -5,16 +5,59 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+new_version=$1
+pattern="([0-9+]).([0-9])+.([0-9])+"
+
+if ! [[ "$new_version" =~ $pattern ]]; then
+    echo "version must be 0.majornum.minornum" >&2
+    exit 1
+fi
+
+version1=${BASH_REMATCH[1]}
+version2=${BASH_REMATCH[2]}
+version3=${BASH_REMATCH[3]}
+branch=release-${version1}.${version2}
+
 ALL_JARS=`pwd`/.tmp/all-jars
 
-version=$(grep version ./pom.xml | tail -1 | sed 's/.*<version>//' | sed 's/<\/version>//')
-target=`pwd`/wikibrain-and-deps-${version}.zip
+old_version=$(grep version ./pom.xml | tail -1 | sed 's/.*<version>//' | sed 's/<\/version>//')
+target=`pwd`/wikibrain-and-deps-${new_version}.zip
 
-rm -rf $ALL_JARS $target
-mvn clean compile package deploy -P release-snapshot -DskipTests=true &&
-mvn dependency:copy-dependencies -DoutputDirectory=$ALL_JARS &&
-cp -p wikibrain*/target/wikibrain*${version}.jar $ALL_JARS &&
-(cd $ALL_JARS && zip $target *.jar) &&
-scp -p $target shilad.com:/var/www/html/www.shilad.com/wikibrain
+echo "current version is $old_version"
+echo "new version will be $new_version"
+echo "new branch will be $branch"
+echo "new zipfile will be $target"
+echo "if all these things seem correct, type 'go'"
+
+read GO
+
+if [ ${GO} != "go" ]; then
+    echo "aborting!"
+fi
+
+rm -rf ${ALL_JARS} ${target}
+
+# prepare release
+git branch ${branch} &&
+mvn clean &&
+mvn versions:set -DnewVersion=${new_version} &&
+mvn  -f ./wikibrain-parent/pom.xml versions:set -DnewVersion=${new_version} &&
+mvn  -N versions:update-child-modules &&
+mvn versions:commit ||
+{ echo "updating versions failed" >&2; exit 1 }
+
+# push release and commit
+mvn clean compile package &&
+git commit -m "Released ${new_version}" &&
+mvn clean deploy -P release-version -DskipTests=true &&
+mvn nexus-staging:release  -P release-version -DskipTests=true ||
+{ echo "Pushing release to git or sonatype failed" >&2; exit 1 }
+
+# zip up full jar
+mvn dependency:copy-dependencies -DoutputDirectory=${ALL_JARS} &&
+cp -p wikibrain*/target/wikibrain*${new_version}.jar ${ALL_JARS} &&
+(cd ${ALL_JARS} && zip ${target} *.jar) &&
+scp -p ${target} shilad.com:/var/www/html/www.shilad.com/wikibrain ||
+{ echo "Uploading master zip failed" >&2; exit 1 }
 
 
--- push-snapshot.sh
+++ push-snapshot.sh
@@ -5,7 +5,7 @@ ALL_JARS=`pwd`/.tmp/all-jars
 version=$(grep version ./pom.xml | tail -1 | sed 's/.*<version>//' | sed 's/<\/version>//')
 target=`pwd`/wikibrain-and-deps-${version}.zip
 
-rm -rf $ALL_JARS
+rm -rf $ALL_JARS $target
 mvn clean compile package deploy -P release-snapshot -DskipTests=true &&
 mvn dependency:copy-dependencies -DoutputDirectory=$ALL_JARS &&
 cp -p wikibrain*/target/wikibrain*${version}.jar $ALL_JARS &&
