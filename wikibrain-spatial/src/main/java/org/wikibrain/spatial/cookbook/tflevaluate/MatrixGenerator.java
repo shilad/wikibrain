73b0089ae47c329f1d7c0ce8b510dadbaa2a2d37
lvonessen <lvonessen@email.arizona.edu>
2014-07-11 12:07:01 -0500

    Moved map visualisation from MatrixGenerator to MapViewer and made it more generalizable.

--- wikibrain-spatial/src/main/java/org/wikibrain/spatial/cookbook/tflevaluate/MatrixGenerator.java
+++ wikibrain-spatial/src/main/java/org/wikibrain/spatial/cookbook/tflevaluate/MatrixGenerator.java
@@ -92,39 +92,6 @@ public class MatrixGenerator {
 //        System.out.println("Finished generating graph distance matrix");
 //        mg.createMatrixFile("graphmatrix_"+mg.language.getLangCode(),matrix);
 
-        final MatrixWithHeader matrix = loadMatrixFile("graphmatrix_"+mg.language.getLangCode());
-
-//        List<Integer> sortedIds = new ArrayList<Integer>();
-//        sortedIds.addAll(matrix.idsInOrder);
-//        final int la = matrix.idsInOrder.indexOf(65);
-//        Collections.sort(sortedIds,new Comparator<Integer>() {
-//            @Override
-//            public int compare(Integer o1, Integer o2) {
-//                int i=matrix.idsInOrder.indexOf(o1);
-//                int j=matrix.idsInOrder.indexOf(o2);
-//                float[] values = {matrix.matrix[la][i],matrix.matrix[i][la],matrix.matrix[la][j],matrix.matrix[j][la]};
-//                for (int k=0; k<4; k++){
-//                    if (values[k]==Float.POSITIVE_INFINITY){
-//                        values[k] = 16;
-//                    }
-//                }
-//
-//                if ((values[0]-values[1])-(values[2]-values[3])!= 0){
-//                    return (int)(-(values[0]-values[1])+(values[2]-values[3]));
-//                }else{
-//                    return (int)(values[1]-values[3]);
-//                }
-//            }
-//        });
-//
-//        for (int i=0; i<20; i++){
-//            System.out.println(sortedIds.get(i)+": "+matrix.matrix[la][matrix.idsInOrder.indexOf(sortedIds.get(i))]+" "+matrix.matrix[matrix.idsInOrder.indexOf(sortedIds.get(i))][la]);
-//        }
-
-//        mg.generateGraphNeighborUrl(matrix,65,Float, 9.POSITIVE_INFINITY);
-        mg.generateGraphNeighborUrl(matrix,118388,7,Float.POSITIVE_INFINITY);
-
-
 
 //        System.out.println("Finished writing matrix to file");
 //        MatrixWithHeader matrix2 = mg.loadMatrixFile("srmatrix");
@@ -149,78 +116,6 @@ public class MatrixGenerator {
 //                System.out.println("Unequal row "+i);
 //            }
 //        }
-    }
-
-    public void generateGraphNeighborUrl (final MatrixWithHeader graph, final int conceptId, int zoom, float max){
-        String[] colors = {"color:purple|","color:blue|","color:green|","color:yellow|","color:orange|",""};
-        String base = "http://maps.googleapis.com/maps/api/staticmap?center=";
-        String base2 = "&zoom="+zoom+"&size=800x524&markers=color:black|";
-        List<Integer> neighbors = new ArrayList<Integer>();
-        neighbors.addAll(geometries.keySet());
-        final int conceptIndex = graph.idToIndex.get(conceptId);
-        Collections.sort(neighbors,new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                float d1 = graph.matrix[conceptIndex][graph.idsInOrder.indexOf(o1)];
-                float d2 = graph.matrix[conceptIndex][graph.idsInOrder.indexOf(o2)];
-                if (d1==Float.POSITIVE_INFINITY)
-                    d1 = 16;
-                if (d2==Float.POSITIVE_INFINITY)
-                    d2 = 16;
-                return (int)(d1-d2);
-            }
-        });
-
-        Point source = (Point) geometries.get(conceptId);
-        String sourceStr = source.getY()+","+source.getX();
-        base+= sourceStr + base2+ sourceStr+"|";
-        System.out.println(sourceStr);
-
-        if (neighbors.get(0)!=conceptId){
-            System.out.println("Error: closest neighbor is not self, "+conceptId+", but "+neighbors.get(0));
-        }
-
-        int i=1;
-        float curDepth = 0;
-        String current = base;
-        int index;
-        do{
-            index = graph.idsInOrder.indexOf(neighbors.get(i));
-            Point p2 = (Point) geometries.get(neighbors.get(i));
-            String p2Str = String.format("%.4f,%.4f|",p2.getY(),p2.getX());
-            float newDepth = graph.matrix[conceptIndex][index];
-
-            // switch color if necessary
-            if (newDepth!=curDepth){
-                if (newDepth == max){
-                    break;
-                }
-                System.out.println("Length: "+(current.length()-1));
-                current = current.substring(0,current.length()-1);
-                p2Str="&markers="+colors[((int)newDepth-1)%colors.length]+p2Str;
-                curDepth = newDepth;
-                System.out.println("Starting level "+curDepth);
-                if (current.length()+p2Str.length()>1000){
-                    System.out.println("Length: "+(current.length()-1));
-                    System.out.println(current);
-                    current = base+p2Str;
-                }else{
-                    current += p2Str;
-                }
-            }
-            else if (current.length()+p2Str.length()>1000){
-                System.out.println("Length: "+(current.length()-1));
-                System.out.println(current.substring(0,current.length()-1));
-                current = base+colors[((int)newDepth-1)%colors.length]+p2Str;
-            }else{
-                current += p2Str;
-            }
-            i++;
-
-        }while(true);//graph.matrix[conceptIndex][index]!=Float.POSITIVE_INFINITY && graph.matrix[conceptIndex][index]<colors.length);
-
-        System.out.println("Length: "+(current.length()-1));
-        System.out.println(current.substring(0,current.length()-1));
     }
 
     public MatrixWithHeader generateDistanceMatrix(){
