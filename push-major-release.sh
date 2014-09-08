a774d0d9b1c62a15dabc57623e811280192b3ea8
shilad <shilad@gmail.com>
2014-09-08 15:54:38 -0500

    Update major release for new versioning scheme.

--- push-major-release.sh
+++ push-major-release.sh
@@ -2,13 +2,13 @@
 
 ALL_JARS=`pwd`/.tmp/all-jars
 
-version=$(grep wikibrain.version wikibrain-parent/pom.xml | head -1  | sed 's/.*<wikibrain.version>//' | sed 's/<\/wikibrain.version>//')
+version=$(grep '<version>' pom.xml | head -2  | tail -1 | sed 's/.*<version>//' | sed 's/<\/version>//')
 echo "version is $version"
 target=`pwd`/wikibrain-withdeps-${version}.zip
 shadedjar=`pwd`/wikibrain/wikibrain-withdeps-${version}.jar
 
 rm -rf $ALL_JARS $target
-mvn clean compile package deploy -P release-version -DskipTests=true &&
+mvn clean compile package -DskipTests=true &&
 mvn dependency:copy-dependencies -DoutputDirectory=$ALL_JARS &&
 cp -p wikibrain*/target/wikibrain*${version}.jar $ALL_JARS &&
 (cd $ALL_JARS && zip $target *.jar) &&
