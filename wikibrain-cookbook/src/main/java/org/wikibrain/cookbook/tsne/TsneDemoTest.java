5ecf4d66f1c9557b38379bf5350305e4b2d81feb
Qisheng Li <qli@macalester.edu>
2016-06-06 14:38:50 -0500

    tsne

--- wikibrain-cookbook/src/main/java/org/wikibrain/cookbook/tsne/TsneDemoTest.java
+++ wikibrain-cookbook/src/main/java/org/wikibrain/cookbook/tsne/TsneDemoTest.java
@@ -20,7 +20,7 @@ import static org.wikibrain.cookbook.tsne.TSneDemo.saveFile;
         public static void main(String [] args) {
             int initial_dims = 50;
             double perplexity = 20.0;
-            double [][] X = MatrixUtils.simpleRead2DMatrix(new File("/Users/research/wikibrain/wikibrain-cookbook/src/main/java/org/wikibrain/cookbook/tsne/data/testing.txt"), ",");
+            double [][] X = MatrixUtils.simpleRead2DMatrix(new File("/Users/research/Desktop/wikibrain/wikibrain-cookbook/src/main/java/org/wikibrain/cookbook/tsne/data/testing.txt"), ", ");
             System.out.println(MatrixOps.doubleArrayToPrintString(X, ", ", 50,10));
             TSne tsne = new FastTSne();
             double [][] Y = tsne.tsne(X, 2, initial_dims, perplexity);
