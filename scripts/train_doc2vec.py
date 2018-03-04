41e7be610b34514b69bd4ed6ec10f3caa1f9f867
Shilad Sen <shilad@gmail.com>
2018-03-03 22:26:16 -0600

    Fix slice for optional is_binary parameter

--- scripts/train_doc2vec.py
+++ scripts/train_doc2vec.py
@@ -122,7 +122,7 @@ if __name__ == '__main__':
         binary = str2bool(sys.argv[4])
 
     logging.basicConfig(level=logging.INFO)
-    (corpus_dir, min_freq, output_path) = sys.argv[1:]
+    (corpus_dir, min_freq, output_path) = sys.argv[1:5]
     min_freq = int(min_freq)
 
     freqs = read_word_freqs(corpus_dir + '/dictionary.txt', min_freq)
