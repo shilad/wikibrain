b7d35c3b297744d2d94a81c23d45b5f2d2641b4a
Ubuntu <ubuntu@ip-10-0-0-143.ec2.internal>
2018-03-14 21:51:28 +0000

    refactored w2v and corpus building.

---
+++ scripts/build_corpus.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+# 1. Imports a language edition of Wikipedia
+# 2. Exports a wikified corpus
+# 4. Uploads the corpus to s3
+#
+
+if [ $# -ne 1 ]; then 
+	echo "usage: $0 wp_lang_code"
+	exit 1
+fi
+
+wp_lang=$1
+wp_db=wikibrain_${wp_lang}
+base_dir=`pwd`/base_${wp_lang}
+java_memory=64000M
+max_threads=10
+
+export PGPASSWORD=wikibrain
+export JAVA_OPTS="-d64 -Xmx${java_memory} -server"
+
+set -e
+set -x
+
+# Set up the environment
+#
+psql -h 127.0.0.1 -U wikibrain -c "CREATE DATABASE $wp_db" postgres || 
+	echo "couldn't create database $wp_db. it probably exists, so we will continue" >&2
+
+[ -d $base_dir ] || mkdir -p $base_dir
+
+cat <<EOF > $base_dir/lang.conf
+baseDir : "${base_dir}"
+dao.dataSource.default : psql
+dao.dataSource.psql {
+    username : wikibrain
+    password : wikibrain
+    connectionsPerPartition : 5
+    url : "jdbc:postgresql://localhost/${wp_db}"
+}
+EOF
+
+mvn -f wikibrain-utils/pom.xml clean compile exec:java -Dexec.mainClass=org.wikibrain.utils.ResourceInstaller
+
+# Import language for wikipedia
+./wb-java.sh org.wikibrain.Loader -l ${wp_lang} -c ${base_dir}/lang.conf -h ${max_threads}
+
+# Build the necessary SR model
+./wb-java.sh org.wikibrain.sr.SRBuilder -m prebuiltword2vec -c ${base_dir}/lang.conf -f -h ${max_threads}
+
+# Create the word2vec corpus
+./wb-java.sh org.wikibrain.sr.wikify.CorpusCreatorMain -p wikified -o ${base_dir}/corpus_w2v --desiredPrecision 0.98 -c ${base_dir}/lang.conf -h ${max_threads}
+
+# Upload to s3
+pbzip2 -v ${base_dir}/corpus_w2v/dictionary.txt
+pbzip2 -v ${base_dir}/corpus_w2v/corpus.txt
+
+aws s3 cp ${base_dir}/corpus_w2v/dictionary.txt.bz2 s3://wikibrain/w2v2/${wp_lang}/
+aws s3 cp ${base_dir}/corpus_w2v/corpus.txt.bz2 s3://wikibrain/w2v2/${wp_lang}/
--- scripts/build_w2v.sh
+++ scripts/build_w2v.sh
@@ -1,63 +1,38 @@
 #!/bin/bash
 #
-# 1. Imports a language edition of Wikipedia
-# 2. Exports a wikified corpus
-# 3. Builds a word2vec model using gensim
-# 4. Uploads the model to s3
+# Rebuilds word2vec models for a particular language
+# usage: build-w2v.sh [model-file-name] [lang1,lang2,..]
+#        build-w2v.sh [model-file-name]
+#        build-w2v.sh
 #
-
-if [ $# -ne 1 ]; then 
-	echo "usage: $0 wp_lang_code"
-	exit 1
-fi
-
-wp_lang=$1
-wp_db=wikibrain_${wp_lang}
-base_dir=`pwd`/base_${wp_lang}
-java_memory=64000M
-
-export PGPASSWORD=wikibrain
-export JAVA_OPTS="-d64 -Xmx${java_memory} -server"
 
 set -e
 set -x
 
-# Set up the environment
-#
-psql -h 127.0.0.1 -U wikibrain -c "CREATE DATABASE $wp_db" postgres || 
-	echo "couldn't create database $wp_db. it probably exists, so we will continue" >&2
+langs=$@
+if [ $# -eq 0 ]; then
+	name=vectors.w2v.bin
+	langs="en,de,simple,ar,az,bg,ca,cs,da,eo,es,et,eu,fa,fi,fr,gl,he,hi,hr,hu,id,it,ja,kk,ko,lt,ms,nl,nn,no,pl,pt,ro,ru,sk,sl,sr,sv,tr,uk,vi,vo,war,zh"
+elif [ $# -eq 1 ]; then
+	name=$1
+	langs="en,de,simple,ar,az,bg,ca,cs,da,eo,es,et,eu,fa,fi,fr,gl,he,hi,hr,hu,id,it,ja,kk,ko,lt,ms,nl,nn,no,pl,pt,ro,ru,sk,sl,sr,sv,tr,uk,vi,vo,war,zh"
+else
+	name=$1
+	langs="$2"
+fi
 
-[ -d $base_dir ] || mkdir -p $base_dir
+echo building word2vec models for languages $langs
 
-cat <<EOF > $base_dir/lang.conf
-baseDir : "${base_dir}"
-dao.dataSource.default : psql
-dao.dataSource.psql {
-    username : wikibrain
-    password : wikibrain
-    connectionsPerPartition : 5
-    url : "jdbc:postgresql://localhost/${wp_db}"
+function do_lang() {
+    lang=$1
+    mkdir -p ./w2v/$lang
+    aws s3 cp s3://wikibrain/w2v2/$lang/corpus.txt.bz2 ./w2v/$lang/
+    aws s3 cp s3://wikibrain/w2v2/$lang/dictionary.txt.bz2 ./w2v/$lang/
+    python3 ./scripts/train_doc2vec.py ./w2v/$lang/ 20 ./w2v/$lang/$name
+    aws s3 cp ./w2v/$lang/$name s3://wikibrain/w2v2/$lang/
+    rm -rf ./w2v/$lang/
 }
-EOF
 
-mvn -f wikibrain-utils/pom.xml clean compile exec:java -Dexec.mainClass=org.wikibrain.utils.ResourceInstaller
+export -f do_lang
 
-# Import language for wikipedia
-./wb-java.sh org.wikibrain.Loader -l ${wp_lang} -c ${base_dir}/lang.conf
-
-# Build the necessary SR model
-./wb-java.sh org.wikibrain.sr.SRBuilder -m prebuiltword2vec -c ${base_dir}/lang.conf -f
-
-# Create the word2vec corpus
-./wb-java.sh org.wikibrain.sr.wikify.CorpusCreatorMain -p wikified -o ${base_dir}/corpus_w2v --desiredPrecision 0.98 -c ${base_dir}/lang.conf
-
-# Train the word2vec model
-python3 ./scripts/train_doc2vec.py ${base_dir}/corpus_w2v 20 ${base_dir}/vectors.${wp_lang}.bin True
-
-# Upload to s3
-pbzip2 -v ${base_dir}/corpus_w2v/dictionary.txt
-pbzip2 -v ${base_dir}/corpus_w2v/corpus.txt
-
-aws s3 cp ${base_dir}/vectors.${wp_lang}.bin s3://wikibrain/w2v2/${wp_lang}/
-aws s3 cp ${base_dir}/corpus_w2v/dictionary.txt.bz2 s3://wikibrain/w2v2/${wp_lang}/
-aws s3 cp ${base_dir}/corpus_w2v/corpus.txt.bz2 s3://wikibrain/w2v2/${wp_lang}/
+echo $langs | tr ',' '\n' | parallel -j 6 --line-buffer do_lang '{}'
